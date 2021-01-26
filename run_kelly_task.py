7b6ff39acb53a504d8a500775bdfd8bea9e708cc
Luke Sjulson <lukesjulson@gmail.com>
2021-01-26 10:27:29 -0500

    renamed launch script run_kelly_task.py and put session_info in a separate file

--- run_kelly_task.py
+++ run_kelly_task.py
@@ -21,23 +21,9 @@ logging.config.dictConfig({
 # import your task class here
 from kelly_task import KellyTask
 
-# defining immutable mouse dict (once defined for a mouse, this should never change)
-mouse_info = pysistence.make_dict({'mouse_name': 'mouse01',
-                 'fake_field': 'fake_info',
-                 })
-
-# Information for this session (the user should edit this each session)
-session_info                              	= collections.OrderedDict()
-session_info['mouse_name']                 	= mouse_info['mouse_name']
-#session_info['trainingPhase']             	= 4
-session_info['basedir']					  	= '/home/pi/fakedata'
-session_info['weight']                	    = 32.18
-session_info['date']                   	   	= datetime.now().strftime("%Y-%m-%d")
-session_info['time']                   	   	= datetime.now().strftime('%H%M%S')
-session_info['datetime']					= session_info['date'] + '_' + session_info['time']
-session_info['basename']                  	= mouse_info['mouse_name'] + '_' + session_info['datetime']
-session_info['box_name']             		= socket.gethostname()
-session_info['dirname']  					= session_info['basedir'] + "/" + session_info['mouse_name'] + "_" + session_info['datetime']
+# import session and mouse info
+from session_info import session_info
+from session_info import mouse_info
 
 
 # make data directory and initialize logfile
---
+++ session_info.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+import os
+import pysistence, collections
+import socket
+
+
+# defining immutable mouse dict (once defined for a mouse, this should never change)
+mouse_info = pysistence.make_dict({'mouse_name': 'mouse01',
+                 'fake_field': 'fake_info',
+                 })
+
+# Information for this session (the user should edit this each session)
+session_info                              	= collections.OrderedDict()
+session_info['mouse_name']                 	= mouse_info['mouse_name']
+#session_info['trainingPhase']             	= 4
+session_info['basedir']					  	= '/home/pi/fakedata'
+session_info['weight']                	    = 32.18
+session_info['date']                   	   	= datetime.now().strftime("%Y-%m-%d")
+session_info['time']                   	   	= datetime.now().strftime('%H%M%S')
+session_info['datetime']					= session_info['date'] + '_' + session_info['time']
+session_info['basename']                  	= mouse_info['mouse_name'] + '_' + session_info['datetime']
+session_info['box_name']             		= socket.gethostname()
+session_info['dirname']  					= session_info['basedir'] + "/" + session_info['mouse_name'] + "_" + session_info['datetime']
+
