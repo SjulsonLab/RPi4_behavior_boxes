cb6148fd46994e9ad81fefe6698178d1898cb571
Luke Sjulson <lukesjulson@gmail.com>
2021-01-26 11:12:41 -0500

    modified session_info file so that it has to be updated for today's date

--- run_kelly_task.py
+++ run_kelly_task.py
@@ -11,6 +11,10 @@ from gpiozero import PWMLED, LED, Button
 import logging.config
 import pysistence, collections
 import socket
+import importlib
+import colorama
+import warnings
+from colorama import Fore, Style
 
 # all modules above this line will have logging disabled
 logging.config.dictConfig({
@@ -21,46 +25,72 @@ logging.config.dictConfig({
 # import your task class here
 from kelly_task import KellyTask
 
-# import session and mouse info
-from session_info import session_info
-from session_info import mouse_info
+# import session and mouse info, contingent upon the dates matching
+full_module_name = 'session_info_' + datetime.now().strftime("%Y-%m-%d")
+tempmod = importlib.import_module(full_module_name)
+session_info = tempmod.session_info
+mouse_info   = tempmod.mouse_info
+
+ic(session_info['manual_date'])
+ic(session_info['date'])
+if session_info['manual_date'] != session_info['date']:  # check if file is updated
+    print('wrong date!!')
+    raise RuntimeError('manual_date field in session_info file is not updated')
 
 
-# make data directory and initialize logfile
-os.makedirs( session_info['dirname'] )
-os.chdir( session_info['dirname'] )
-filename = session_info['mouse_name'] + "_" + session_info['datetime'] + ".log" 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s.%(msecs)03d,[%(levelname)s],%(message)s",
-    datefmt=('%Y-%m-%d,%H:%M:%S'),
-    handlers=[
-        logging.FileHandler(filename),
-        logging.StreamHandler()
-    ]
-)
+try:
 
-# initiate task object
-task = KellyTask("fentanyl")
+    # make data directory and initialize logfile
+    os.makedirs( session_info['dirname'] )
+    os.chdir( session_info['dirname'] )
+    filename = session_info['mouse_name'] + "_" + session_info['datetime'] + ".log" 
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s.%(msecs)03d,[%(levelname)s],%(message)s",
+        # datefmt=('%Y-%m-%d,%H:%M:%S'),
+        datefmt=('%H:%M:%S'),
+        handlers=[
+            logging.FileHandler(filename),
+            logging.StreamHandler()
+        ]
+    )
 
-
-# start session
-task.start_session()
-
-
-# loop over trials
-for i in range(3):
+    # initiate task object
+    task = KellyTask("fentanyl")
 
 
-	logging.info("starting trial")
-	print("starting trial")
+    # start session
+    task.start_session()
 
-	task.trial_start()
 
-	while task.trial_running:
-		task.run()
+    # loop over trials
+    for i in range(3):
 
-task.end_session()
+
+        logging.info("starting trial")
+        print("starting trial")
+
+        task.trial_start()
+
+        while task.trial_running:
+            task.run()
+
+    task.end_session()
+    raise SystemExit
+
+# graceful exit
+except (KeyboardInterrupt, SystemExit):
+    print(Fore.RED + Style.BRIGHT + 'Exiting now' + Style.RESET_ALL)
+    # save dicts to disk
+    import scipy.io 
+    # scipy.io.savemat(filename, {struct_name: dict_to_save})
+    # box_utils.save_mat_file('mouse_info.mat', mouse_info, 'mouse_info')
+    # box_utils.save_mat_file('session_info.mat', session_info, 'session_info')
+    scipy.io.savemat('mouse_info.mat', {'mouse_info': mouse_info})
+    scipy.io.savemat('session_info.mat', {'session_info': session_info})
+
+
+
 
 
 
--- session_info_2021-01-26.py
+++ session_info_2021-01-26.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# put all of your mouse and session info in here
+
 from datetime import datetime
 import os
 import pysistence, collections
@@ -17,10 +19,12 @@ session_info['mouse_name']                 	= mouse_info['mouse_name']
 #session_info['trainingPhase']             	= 4
 session_info['basedir']					  	= '/home/pi/fakedata'
 session_info['weight']                	    = 32.18
+session_info['manual_date']					= '2021-01-26'
 session_info['date']                   	   	= datetime.now().strftime("%Y-%m-%d")
 session_info['time']                   	   	= datetime.now().strftime('%H%M%S')
 session_info['datetime']					= session_info['date'] + '_' + session_info['time']
 session_info['basename']                  	= mouse_info['mouse_name'] + '_' + session_info['datetime']
 session_info['box_name']             		= socket.gethostname()
 session_info['dirname']  					= session_info['basedir'] + "/" + session_info['mouse_name'] + "_" + session_info['datetime']
+
 
