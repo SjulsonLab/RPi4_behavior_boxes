b93e634d93b173b93c175778a7cd7c122d8b1726
Luke Sjulson <lukesjulson@gmail.com>
2021-02-24 06:01:35 -0500

    used black to reformat kelly_task.py

--- kelly_task.py
+++ kelly_task.py
@@ -9,10 +9,13 @@ import os
 from gpiozero import PWMLED, LED, Button
 from colorama import Fore, Style
 import logging.config
-logging.config.dictConfig({
-    'version': 1,
-    'disable_existing_loggers': True,
-})
+
+logging.config.dictConfig(
+    {
+        "version": 1,
+        "disable_existing_loggers": True,
+    }
+)
 # all modules above this line will have logging disabled
 
 import behavbox
@@ -22,62 +25,84 @@ import behavbox
 class TimedStateMachine(Machine):
     pass
 
-class KellyTask(object):
 
-    def __init__(self, **kwargs):  # name and session_info should be provided as kwargs 
+class KellyTask(object):
+    def __init__(self, **kwargs):  # name and session_info should be provided as kwargs
 
         # if no name or session, make fake ones (for testing purposes)
-        if kwargs.get('name', None) is None:
-            self.name = 'name'
-            print(Fore.RED + Style.BRIGHT + 'Warning: no name supplied; making fake one' + Style.RESET_ALL)
-        else: 
-            self.name=kwargs.get('name', None)
+        if kwargs.get("name", None) is None:
+            self.name = "name"
+            print(
+                Fore.RED
+                + Style.BRIGHT
+                + "Warning: no name supplied; making fake one"
+                + Style.RESET_ALL
+            )
+        else:
+            self.name = kwargs.get("name", None)
 
-        if kwargs.get('session_info', None) is None:
-            print(Fore.RED + Style.BRIGHT + 'Warning: no session_info supplied; making fake one' + Style.RESET_ALL)
+        if kwargs.get("session_info", None) is None:
+            print(
+                Fore.RED
+                + Style.BRIGHT
+                + "Warning: no session_info supplied; making fake one"
+                + Style.RESET_ALL
+            )
             from fake_session_info import fake_session_info
+
             self.session_info = fake_session_info
-        else: 
-            self.session_info = kwargs.get('session_info', None)
+        else:
+            self.session_info = kwargs.get("session_info", None)
         ic(self.session_info)
 
         ########################################################################
         # Task has three possible states: standby, reward_available, and cue
         ########################################################################
         self.states = [
-            State(name='standby', on_enter=['enter_standby'], on_exit=['exit_standby']),
-            Timeout(name='reward_available', on_enter=['enter_reward_available'], on_exit=['exit_reward_available']),
-            Timeout(name='cue', on_enter=['enter_cue'], on_exit=['exit_cue'], timeout=self.session_info['timeout_length'], 
-                on_timeout=['timeup'])
+            State(name="standby", on_enter=["enter_standby"], on_exit=["exit_standby"]),
+            Timeout(
+                name="reward_available",
+                on_enter=["enter_reward_available"],
+                on_exit=["exit_reward_available"],
+            ),
+            Timeout(
+                name="cue",
+                on_enter=["enter_cue"],
+                on_exit=["exit_cue"],
+                timeout=self.session_info["timeout_length"],
+                on_timeout=["timeup"],
+            ),
         ]
         # can set later with task.machine.states['cue'].timeout etc.
-
 
         ########################################################################
         # list of possible transitions between states
         # format is: [event_name, source_state, destination_state]
         ########################################################################
         self.transitions = [
-            ['trial_start', 'standby', 'reward_available'],
-            ['active_poke', 'reward_available', 'cue'],
-            ['timeup', 'cue', 'standby']
+            ["trial_start", "standby", "reward_available"],
+            ["active_poke", "reward_available", "cue"],
+            ["timeup", "cue", "standby"],
         ]
-
 
         ########################################################################
         # initialize state machine and behavior box
         ########################################################################
-        self.machine = TimedStateMachine(model=self, states=self.states, transitions=self.transitions, 
-            initial='standby')
+        self.machine = TimedStateMachine(
+            model=self,
+            states=self.states,
+            transitions=self.transitions,
+            initial="standby",
+        )
         self.trial_running = False
 
         # initialize behavior box
         self.box = behavbox.BehavBox(self.session_info)
 
     ########################################################################
-    # functions called when state transitions occur 
+    # functions called when state transitions occur
     ########################################################################
-    def enter_standby(self): 
+    def enter_standby(self):
         print("entering standby")
         self.trial_running = False
 
@@ -94,10 +119,10 @@ class KellyTask(object):
 
     def enter_cue(self):
         print("deliver reward")
-        self.box.reward('left', self.session_info['reward_size'])
+        self.box.reward("left", self.session_info["reward_size"])
         print("start cue")
         self.box.cueLED1.on()
-        self.box.visualstim.show_grating('first_grating.grat')
+        self.box.visualstim.show_grating("first_grating.grat")
 
     def exit_cue(self):
         print("stop cue")
@@ -105,7 +130,7 @@ class KellyTask(object):
 
     ########################################################################
     # call the run() method repeatedly in a while loop in the main session
-    # script it will process all detected events from the behavior box (e.g. 
+    # script it will process all detected events from the behavior box (e.g.
     # nosepokes and licks) and trigger the appropriate state transitions
     ########################################################################
     def run(self):
@@ -114,31 +139,28 @@ class KellyTask(object):
         if self.box.event_list:
             event_name = self.box.event_list.popleft()
         else:
-            event_name = ''
+            event_name = ""
 
-        if self.state=='standby':
+        if self.state == "standby":
             pass
 
-        elif self.state=='reward_available':
-            if event_name=='left_poke_entry':
-                self.active_poke()  # triggers state transition 
+        elif self.state == "reward_available":
+            if event_name == "left_poke_entry":
+                self.active_poke()  # triggers state transition
 
-        elif self.state=='cue':
+        elif self.state == "cue":
             pass
 
         # look for keystrokes
         self.box.check_keybd()
 
-
-
     ########################################################################
     # methods to start and end the behavioral session
     ########################################################################
     def start_session(self):
-        ic('TODO: start video')
+        ic("TODO: start video")
         self.box.video_start()
 
     def end_session(self):
-        ic('TODO: stop video')
+        ic("TODO: stop video")
         self.box.video_stop()
-
