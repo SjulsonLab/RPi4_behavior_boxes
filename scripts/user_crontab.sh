067d9685993ae012935bcf086ee96c15373e7d1c
seasonqiu <season.qiu@yahoo.com>
2021-06-09 19:51:21 -0400

    Edited user-crontab.sh

--- scripts/user_crontab.sh
+++ scripts/user_crontab.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # the user's crontab that runs daily at 3AM
-echo "user crontab run at " `date`
-# REPO_DIR='/home/pi/RPi4_behavior_boxes'
-# GIT_PATH='$which git'
-# cd REPO_DIR
-# /usr/local/bin/git pull origin master
+# echo "user crontab run at " `date`
+REPO_DIR='/home/pi/RPi4_behavior_boxes'
+cd REPO_DIR && git checkout season-crontab && git pull origin main
