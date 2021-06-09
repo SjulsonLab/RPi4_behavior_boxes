bc5eb573704d2d2a4e7c7d8c7a7f889aae6af3da
seasonqiu <season.qiu@yahoo.com>
2021-06-09 18:34:05 -0400

    Add crontab scripts

---
+++ scripts/user_crontab.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# the user's crontab that runs daily at 3AM
+echo "user crontab run at " `date`
+# REPO_DIR='/home/pi/RPi4_behavior_boxes'
+# GIT_PATH='$which git'
+# cd REPO_DIR
+# /usr/local/bin/git pull origin master
