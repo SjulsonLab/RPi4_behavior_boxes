c04d7b654c5dd4ebb646c8992d47dc57e25d831b
seasonqiu <season.qiu@yahoo.com>
2021-07-01 13:16:31 -0400

    Modified user crontab

--- scripts/user_crontab.sh
+++ scripts/user_crontab.sh
@@ -3,3 +3,5 @@
 # the user's crontab that runs daily at 3AM
 echo "user crontab run at " `date`
 cd /home/pi/RPi4_behavior_boxes  && git checkout main && git pull origin main
+
+#rsync --ignore-existing -raz /mnt/hd pi@192.168.1.90:~/videos
