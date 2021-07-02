8ed16f1619676467b9552291de2df6c0c4274f43
season <season.qiu@yahoo.com>
2021-07-02 15:23:39 -0400

    Updated behavbox.py for log issue

--- behavbox.py
+++ behavbox.py
@@ -272,9 +272,9 @@ class BehavBox(object):
         hd_dir = base_dir + basename
         os.mkdir(hd_dir)
 
-        scipy.io.savemat(hd_dir + '_session_info.mat', {'session_info': self.session_info})
+        scipy.io.savemat(hd_dir + basename + '_session_info.mat', {'session_info': self.session_info})
         print("dumping session_info")
-        pickle.dump(self.session_info, open(hd_dir + '_session_info.pkl', "wb"))
+        pickle.dump(self.session_info, open(hd_dir + basename + '_session_info.pkl', "wb"))
 
     def video_stop(self):
         # Get the basename from the session information
@@ -293,11 +293,11 @@ class BehavBox(object):
         # Create a directory for storage on the hard drive mounted on the box behavior
         base_dir = '/mnt/hd/'
         hd_dir = base_dir + basename
-        os.mkdir(hd_dir)
+        # os.mkdir(hd_dir)
 
-        scipy.io.savemat(file_name + '_session_info.mat', {'session_info': self.session_info})
+        scipy.io.savemat(hd_dir + basename + '_session_info.mat', {'session_info': self.session_info})
         print("dumping session_info")
-        pickle.dump(self.session_info, open(hd_dir + '_session_info.pkl', "wb"))
+        pickle.dump(self.session_info, open(hd_dir + basename + '_session_info.pkl', "wb"))
 
         # Move the video + log from the box_video SD card to the box_behavior external hard drive
         os.system(
