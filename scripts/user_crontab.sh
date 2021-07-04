b77512f182c58069d6ef998a4bf0826065185b3e
season <season.qiu@yahoo.com>
2021-07-04 17:00:05 -0400

    Update cron script for all branches

--- scripts/user_crontab.sh
+++ scripts/user_crontab.sh
@@ -3,3 +3,5 @@
 # the user's crontab that runs daily at 3AM
 echo "user crontab run at " `date`
 cd /home/pi/RPi4_behavior_boxes  && git checkout main && git pull origin main
+git checkout video && git pull origin video
+git checkout video_sync && git pull origin video_sync
