2d864b70a46e90f6302c59de38214c396cd9ca27
Season Qiu <season.qiu@yahoo.com>
2021-07-04 14:01:41 -0700

    Update user_crontab.sh
    Commented out video_sync

--- scripts/user_crontab.sh
+++ scripts/user_crontab.sh
@@ -4,4 +4,4 @@
 echo "user crontab run at " `date`
 cd /home/pi/RPi4_behavior_boxes  && git checkout main && git pull origin main
 git checkout video && git pull origin video
-git checkout video_sync && git pull origin video_sync
+# git checkout video_sync && git pull origin video_sync
