7bb1a18915a5eb109b78139a268ed4ffe6dedd7a
seasonqiu <season.qiu@yahoo.com>
2021-07-29 17:24:58 -0400

    Update origin branch with debugged files

--- run_record_task.py
+++ run_record_task.py
@@ -45,26 +45,13 @@ try:
     session_info['time'] = timestr
     session_info['datetime'] = session_info['date'] + '_' + session_info['time']
     session_info['basename'] = session_info['mouse_name'] + '_' + session_info['datetime']
+
+
     session_info['dir_name'] = session_info['basedir'] + "/" + session_info['mouse_name'] + "_" + session_info['datetime']
 
     if session_info['manual_date'] != session_info['date']:  # check if file is updated
         print('wrong date!!')
         raise RuntimeError('manual_date field in session_info file is not updated')
-
-
-    # # make data directory and initialize logfile
-    # os.makedirs(session_info['dir_name'])
-    # os.chdir(session_info['dir_name'])
-    session_info['file_basename'] = session_info['mouse_name'] + "_" + session_info['datetime']
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s.%(msecs)03d,[%(levelname)s],%(message)s",
-        datefmt=('%H:%M:%S'),
-        handlers=[
-            logging.FileHandler(session_info['file_basename'] + '.log'),
-            logging.StreamHandler() # sends copy of log output to screen
-        ]
-    )
 
     # initiate task object\
     task = KellyRecordTask(name="fentanyl_task", session_info=session_info)
@@ -73,12 +60,22 @@ try:
     print("start_session")
     duration = int(input("Enter the time in seconds: "))
     task.start_session()
-    # scipy.io.savemat(session_info['file_basename'] + '_session_info.mat', {'session_info' : session_info})
-    # print("dumping session_info")
-    # pickle.dump(session_info, open( session_info['file_basename'] + '_session_info.pkl', "wb" ) )
     sleep(duration)
-    task.start_session()
     task.end_session()
+    dir_name = session_info['dir_name']
+    basename = session_info['basename']
+    file_name = dir_name + "/" + basename
+    base_dir = '/mnt/hd/'
+    hd_dir = base_dir + basename
+
+    # Per Kelly's request, remove all the files except the video file from the hard drive
+    print("Remove mat - ")
+    os.system("rm -r " + hd_dir + "/*.mat")
+    print("Remove pkl - ")
+    os.system("rm -r " + hd_dir + "/*.pkl")
+    print("Remove log -")
+    os.system("rm -r " + hd_dir + "/*.log")
+
 # graceful exit
 except (KeyboardInterrupt, SystemExit):
     print(Fore.RED + Style.BRIGHT + 'Exiting now...' + Style.RESET_ALL)
@@ -92,6 +89,6 @@ except (KeyboardInterrupt, SystemExit):
 # exit because of error
 except (RuntimeError) as ex:
     print(Fore.RED + Style.BRIGHT + 'ERROR: Exiting now' + Style.RESET_ALL)
-    # save dicts to disk
-    scipy.io.savemat(session_info['file_basename'] + '_session_info.mat', {'session_info' : session_info})
-    pickle.dump( session_info, open( session_info['file_basename'] + '_session_info.pkl', "wb" ) )
+    # # save dicts to disk
+    # scipy.io.savemat(session_info['file_basename'] + '_session_info.mat', {'session_info' : session_info})
+    # pickle.dump( session_info, open( session_info['file_basename'] + '_session_info.pkl', "wb" ) )
