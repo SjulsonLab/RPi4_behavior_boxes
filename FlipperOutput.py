d16f5a04ef9ddef7abd7685fdf5b26bd2e02fa2e
season <season.qiu@yahoo.com>
2021-09-27 15:53:25 -0400

    FlipperOutput.py: def flipper_flush

--- FlipperOutput.py
+++ FlipperOutput.py
@@ -35,7 +35,7 @@ class FlipperOutput(DigitalOutputDevice):
     def close(self):
         self._flip_thread.stopping.set()
         print("Attempts to close!")
-        # self._flip_thread.join() # joining of the thread prevented the rest of the stop code
+        self._flip_thread.join(5) # joining of the thread prevented the rest of the stop code
         self._flip_thread = None
         # print("Attempts to stop!")
         self._stop_flip()
@@ -48,7 +48,7 @@ class FlipperOutput(DigitalOutputDevice):
         # self._controller = None
         print("Entered _stop_flip")
         if getattr(self, '_flip_thread', None):
-            self._flip_thread.join(5)
+            # self._flip_thread.join(5) # CHANGED
             self._flip_thread.stop()
             # self.flipper_flush()
         self._flip_thread = None
