7729f26be059e8dd4630c0a4b1250b68f38c5901
season <season.qiu@yahoo.com>
2021-09-27 11:00:19 -0400

    FlipperOutput.py and run_flip_task.py

--- FlipperOutput.py
+++ FlipperOutput.py
@@ -16,26 +16,31 @@ class FlipperOutput(DigitalOutputDevice):
         # Additional properties and methods
         self._flip_thread = None
         # self._controller = None # what is this?
-        self._flipper_file = self.session_info['flipper_filename'] + self.session_info['datetime'] + 'txt'
+        self._flipper_file = self.session_info['flipper_filename'] + self.session_info['datetime'] + '.txt'
         self._flipper_timestamp = []
 
+        # # experiment
+        # self._output = io.open(file_name, 'wb', buffering=0)
+        # self._event = Event()
+        # self._queue = Queue()
     def flip(self, time_min=0.5, time_max=2, n=None, background=True):
         self._stop_flip()
         self._flip_thread = Thread(
             target=self._flip_device, args=(time_min, time_max, n)
         )
-        # self._flip_thread.stopping = Event()
-        self._flip_thread.daemon = True
+        self._flip_thread.stopping = Event()
+        # self._flip_thread.daemon = True
         self._flip_thread.start()
         if not background:
             self._flip_thread.join()
             self._flip_thread = None
 
     def close(self):
+        self._flip_thread.stopping.set()
         self._flip_thread.join()
         self._flip_thread = None
         self._stop_flip()
-        super().close()
+        # super().close()
 
     def _stop_flip(self):
         # if getattr(self, '_controller', None):
@@ -48,22 +53,29 @@ class FlipperOutput(DigitalOutputDevice):
 
     def _flip_device(self, time_min, time_max, n):
         iterable = repeat(0) if n is None else repeat(0, n)
-        self._flip_thread.stopping = Event()
+        # self._flip_thread.stopping = Event()
         for _ in iterable:
             self._flip_thread.stopping.clear()
             on_time = round(random.uniform(time_min, time_max), 3)
             off_time = round(random.uniform(time_min, time_max), 3)
+
             self._write(True)
+            timestamp = (self.pin_state, time.time())
+            print(str(timestamp))
+            self._flipper_timestamp.append(timestamp)
             if self._flip_thread.stopping.wait(on_time):
-                self._flipper_timestamp.append((self.pin_state, time.time(), time.clock_gettime(time.CLOCK_REALTIME)))
                 break
+
             self._write(False)
+            timestamp = (self.pin_state, time.time())
+            print(str(timestamp))
+            self._flipper_timestamp.append(timestamp)
             if self._flip_thread.stopping.wait(off_time):
-                self._flipper_timestamp.append((self.pin_state, time.time(), time.clock_gettime(time.CLOCK_REALTIME)))
                 break
+
 
     def flipper_flush(self):
         with io.open(self._flipper_file, 'w') as f:
-            f.write('pin_tate, time.time(), clock_realtime\n')
+            f.write('pin_tate, time.time()\n')
             for entry in self._flipper_timestamp:
-                f.write('%f,%f,%f\n' % entry)
+                f.write('%f,%f\n' % entry)
--- run_flip_task.py
+++ run_flip_task.py
@@ -1,6 +1,15 @@
 from FlipperOutput import FlipperOutput
 from fake_session_info import fake_session_info
 from time import sleep
+import sys
+
+# def signal_handler(signum):
+#     print("SIGINT detected")
+#     FlipperOutput.close()
+#     print('Closing the output.')
+#     sys.exit(0)
+#
+# signal.signal(signal.SIGINT, signal_handler)
 
 with FlipperOutput(fake_session_info, pin=22) as flip_box:
     try:
