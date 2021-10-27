9527eb52f14cd6c002ec21a11e726a6cba9860ed
Duy Tran <duy.tran.einsteinmed@gmail.com>
2021-10-27 09:59:02 -0400

    Update vstest.py

--- visual_stimuli/vstest.py
+++ visual_stimuli/vstest.py
@@ -2,7 +2,7 @@
 ################
 import rpg
 options = {"duration": 3, "angle": 45, "spac_freq": 0.4, "temp_freq": 2}
-rpg.build_grating("~/second_grating.dat", options)
+rpg.build_grating("~/first_grating_ssrt.dat", options)
 with rpg.Screen() as myscreen:
- grating = myscreen.load_grating("~/second_grating.dat")  
+ grating = myscreen.load_grating("~/first_grating_ssrt.dat")
  myscreen.display_grating(grating)
