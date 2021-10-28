2e4c0dde68efb6eaf5e85cf3b5db2c22f6678a19
Duy Tran <duy.tran.einsteinmed@gmail.com>
2021-10-28 09:59:03 -0400

    update

--- ssrt_session_info_year-month-date.py
+++ ssrt_session_info_year-month-date.py
@@ -29,10 +29,12 @@ session_info['box_name']             		= socket.gethostname()
 session_info['config']						= 'head_fixed_v1'
 
 # behavior parameters
-trial_length                                = 3  # in seconds
+session_info['trial_length']                = 3  # in seconds
 session_info['init_length']                 = 1  # in seconds
 session_info['lockout_length']              = 0.2  # in seconds
-session_info['reward_available_length']     = trial_length - session_info['lockout_length']
+session_info['vacuum_length']               = 0.1  # in seconds
+session_info['reward_available_length']     = session_info['trial_length'] - session_info['lockout_length'] - session_info['vacuum_length']
+session_info['lick_count_length']           = 3  # in seconds (default is 3, but will be changed during task)
 session_info['reward_size']					= 10  # in microliters
 session_info['iti_length']                  = 3  # in seconds
 
--- ssrt_task_phase1_v1.py
+++ ssrt_task_phase1_v1.py
@@ -9,6 +9,7 @@ import os
 from gpiozero import PWMLED, LED, Button
 from colorama import Fore, Style
 import logging.config
+import time
 
 logging.config.dictConfig(
     {
@@ -56,37 +57,57 @@ class ssrt_task(object):
         ic(self.session_info)
 
         ########################################################################
-        # Task has 5 possible states: standby, initiation, stim, reward_available, and iti
+        # Task has many possible states
         ########################################################################
         self.states = [
             State(name="standby", on_enter=["enter_standby"], on_exit=["exit_standby"]),
+            # initiation state: LED light is turned ON for 1s
             Timeout(
                 name="initiation",
                 on_enter=["enter_initiation"],
                 on_exit=["exit_initiation"],
                 timeout=self.session_info["init_length"],
-                on_timeout=["abort_init"],
+                on_timeout=["start_vstim"],
             ),
+            # vstim state: start vstim display (automatic once start, can move on to the next state)
             Timeout(
                 name="vstim",
                 on_enter=["enter_vstim"],
                 on_exit=["exit_vstim"],
                 timeout=self.session_info["lockout_length"],
-                on_timeout=["reward_vstim"],
+                on_timeout=["start_reward"],
             ),
+            # reward_available state: if there is a lick, deliver water then transition to the next state
             Timeout(
                 name="reward_available",
                 on_enter=["enter_reward_available"],
                 on_exit=["exit_reward_available"],
                 timeout=self.session_info["reward_available_length"],
-                on_timeout=["abort_vstim"],
+                on_timeout=["start_vacuum_from_reward_available"],
             ),
+            # lick_count state: licks are logged
+            Timeout(
+                name="lick_count",
+                on_enter=["enter_lick_count"],
+                on_exit=["exit_lick_count"],
+                timeout=self.session_info["lick_count_length"],
+                on_timeout=["start_vacuum_from_lick_count"],
+            ),
+            # vacuum state: open vacuum for specified amount of time (right before trial ends)
+            Timeout(
+                name="vacuum",
+                on_enter=["enter_vacuum"],
+                on_exit=["exit_vacuum"],
+                timeout=self.session_info["vacuum_length"],
+                on_timeout=["start_iti"],
+            ),
+            # iti state
             Timeout(
                 name="iti",
                 on_enter=["enter_iti"],
                 on_exit=["exit_iti"],
                 timeout=self.session_info["iti_length"],
-                on_timeout=["abort_iti"],
+                on_timeout=["return_to_standby"],
             ),
         ]
         # can set later with task.machine.states['cue'].timeout etc.
@@ -97,10 +118,13 @@ class ssrt_task(object):
         ########################################################################
         self.transitions = [
             ["trial_start", "standby", "initiation"],
-            ["abort_init", "initiation", "vstim"],
-            ["reward_vstim", "vstim", "reward_available"],
-            ["abort_vstim", "reward_available", "iti"],
-            ["abort_iti", "iti", "standby"],
+            ["start_vstim", "initiation", "vstim"],
+            ["start_reward", "vstim", "reward_available"],
+            ["start_lick_count", "reward_available", "lick_count"],
+            ["start_vacuum_from_reward_available", "reward_available", "vacuum"],
+            ["start_vacuum_from_lick_count", "lick_count", "vacuum"],
+            ["start_iti", "vacuum", "iti"],
+            ["return_to_standby", "iti", "standby"],
         ]
 
         ########################################################################
@@ -131,30 +155,41 @@ class ssrt_task(object):
     def enter_initiation(self):
         self.trial_running = True
         print("entering initiation")
-        print("port LED ON!")
         self.box.cueLED1.on()
+        print("LED ON!")
 
     def exit_initiation(self):
-        print("port LED OFF!")
+        print("LED OFF!")
         self.box.cueLED1.off()
 
     def enter_vstim(self):
-        print("display GO vstim, enter lockout period")
+        print("displaying vstim")
         self.box.visualstim.show_grating(list(self.box.visualstim.gratings)[0])
 
     def exit_vstim(self):
-        print("GO vstim playing,  exit lockout period")
+        print("transitioning to reward_available")
 
     def enter_reward_available(self):
-        print("deliver reward if lick detected")
+        print("entering reward_available")
 
     def exit_reward_available(self):
-        # turn vacuum ON at the end of the trial
+        print("exiting reward_available")
+
+    def enter_lick_count(self):
+        print("entering lick_count")
+
+    def exit_lick_count(self):
+        print("exiting lick_count")
+
+    def enter_vacuum(self):
         self.box.vacuum_on()
-        print("exit vstim")
+        print("entering vacuum")
+
+    def exit_vacuum(self):
+        self.box.vacuum_off()
+        print("exiting vacuum")
 
     def enter_iti(self):
-        self.box.vacuum_off()
         print("entering ITI")
 
     def exit_iti(self):
@@ -184,11 +219,21 @@ class ssrt_task(object):
 
         elif self.state == "reward_available":
             # Deliver reward from left pump if there is a lick detected on the left port
-            if event_name == "first_lick_detected":
+            self.time_initial = time.time()
+            if event_name == "left_IR_entry":
                 self.pump.reward("left", self.session_info["reward_size"])
-                print("delivering reward")
+                print("delivering reward!!")
+                self.time_elapsed = time.time() - self.time_initial
+                print("time elapsed at transition to lick_count: " + str(self.time_elapsed))
+                self.start_lick_count()  # trigger state transition to lick_count
             else:
-                pass
+                    pass
+
+        elif self.state == "lick_count":
+            self.machine.states['lick_count'].timeout = self.session_info["reward_available_length"] - self.time_elapsed
+
+        elif self.state == "vacuum":
+            pass
 
         elif self.state == "iti":
             pass
