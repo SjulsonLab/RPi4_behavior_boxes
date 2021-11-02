d49caba33ab555c653fe3a116dd17c1ce92d81fc
seasonqiu <seaosn.qiu@yahoo.com>
2021-11-02 15:50:29 -0400

    pump_debug.py pump debug

--- pump_debug.py
+++ pump_debug.py
@@ -1,7 +1,9 @@
 # pump_debug.py: it's for testing the pump class and the pump itself
+import sys
+
 from behavbox import Pump
 from fake_session_info import fake_session_info
-
+side = str(sys.argv[1])
 pump = Pump()
 session_info = fake_session_info
-pump.reward("left", session_info["reward_size"])
+pump.reward(side, session_info["reward_size"])
