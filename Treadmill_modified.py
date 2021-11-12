0600e346902f2910fd37d586d6d63d461730aafe
seasonqiu <seaosn.qiu@yahoo.com>
2021-11-12 14:17:35 -0500

    treadmill debug

--- Treadmill_modified.py
+++ Treadmill_modified.py
@@ -13,9 +13,9 @@ def dacval(bus, address):
     block = bus.read_i2c_block_data(address, 1)
     n = struct.unpack("<l", bytes(block[:4]))[0]
     dvl = n / 100
-    # while n != -1:
-    while True:
+    while n != -1:
         time.sleep(0.005)
         print(str(dvl))
         return dvl
-dacval(bus_i2c, address_i2c)
+while True:
+    dacval(bus_i2c, address_i2c)
