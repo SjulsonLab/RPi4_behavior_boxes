4d3866f703586eb44202899d3901a9ff707a933a
Duy Tran <duy.tran.einsteinmed@gmail.com>
2021-11-20 15:20:12 -0500

    update

--- ssrt_test_behavbox.py
+++ ssrt_test_behavbox.py
@@ -5,20 +5,13 @@ box = behavbox_DT
 
 test_running = True
 while test_running:
-    # reward_size = int(input("What is the reward size: \n"))
-    # pump.reward("left", reward_size)  # currently using left pump for the task
-    # ask_continue = input('Do you want to continue? (y or n) \n')
-    # if ask_continue == "n":
-    #     test_running = False
-    #     break
-
-    sound_duration = int(input("What is the sound duration: \n"))
-    box.sound1.on()
-
+    reward_size = int(input("What is the reward size: \n"))
+    pump.reward("left", reward_size)  # currently using left pump for the task
     ask_continue = input('Do you want to continue? (y or n) \n')
     if ask_continue == "n":
         test_running = False
         break
+
 
 
 
