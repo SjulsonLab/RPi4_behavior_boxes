c9ed78609095d55324db676cd004d4516b0e3cb6
season <season.qiu@yahoo.com>
2021-11-22 09:47:32 -0500

    scripts/user_crontab.sh edited the crontab script

--- scripts/user_crontab.sh
+++ scripts/user_crontab.sh
@@ -3,6 +3,8 @@
 # the user's crontab that runs daily at 3AM
 echo "user crontab run at " `date`
 cd /home/pi/RPi4_behavior_boxes  && git checkout main && git pull origin main
+# Gives permission to every file
+sudo chmod -R 777 /home/pi/RPi4_behavior_boxes
 
 #rsync --ignore-existing -raz /mnt/hd pi@192.168.1.90:~/videos
 #git checkout video && git pull origin video
