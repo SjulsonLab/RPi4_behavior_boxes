13aecf920c8b79b6b94936875823e1559cc2a870
Duy Tran <duy.tran.einsteinmed@gmail.com>
2021-11-30 15:18:02 -0500

    update

--- ssrt_test_behavbox.py
+++ ssrt_test_behavbox.py
@@ -2,16 +2,14 @@
 import behavbox_DT as box
 pump = box.Pump()
 
-test_running = True
-while test_running:
-    reward_size = int(input("What is the reward size: \n"))
-    pump.reward("left", reward_size)  # currently using left pump for the task
-    ask_continue = input('Do you want to continue? (y or n) \n')
-    if ask_continue == "n":
-        test_running = False
-        break
-
-
+# test_running = True
+# while test_running:
+#     reward_size = int(input("What is the reward size: \n"))
+#     pump.reward("left", reward_size)  # currently using left pump for the task
+#     ask_continue = input('Do you want to continue? (y or n) \n')
+#     if ask_continue == "n":
+#         test_running = False
+#         break
 
 
 
