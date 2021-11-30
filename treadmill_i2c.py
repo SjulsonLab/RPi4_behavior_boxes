29965781be94243ac3aadeec574a263cfb3a434f
season <season.qiu@yahoo.com>
2021-11-30 14:35:54 -0500

    work in progress for the Treadmil function

---
+++ treadmill_i2c.py
@@ -0,0 +1,24 @@
+import smbus
+import time
+import struct
+# source ="https://gist.github.com/gileri/5a9285d6a1cfde142260.js"
+
+# for RPI version 1, use "bus = smbus.SMBus(0)"
+bus = smbus.SMBus(1)
+
+# This is the address we setup in the Arduino Program
+address = 0x08
+
+def get_data():
+    return bus.read_i2c_block_data(address, 0);
+
+def get_float(data, index):
+    bytes = data[4*index:(index+1)*4]
+    return struct.unpack('f', "".join(map(chr, bytes)))[0]
+
+while True:
+    try:
+        data = get_data()
+    except:
+        continue
+    time.sleep(0.3)
