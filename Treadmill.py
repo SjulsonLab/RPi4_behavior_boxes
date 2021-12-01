6d254fe7fcd1eecfb09c9bc56ad3c1d820155176
season <season.qiu@yahoo.com>
2021-12-01 15:24:15 -0500

    Treadmill.py: unpack binary structure test

--- Treadmill.py
+++ Treadmill.py
@@ -8,10 +8,14 @@ bus = smbus.SMBus(1) # "On all recent (since 2014) raspberries the GPIO pin's I2
 address = 0x08
 
 
-def dacval():
+def dacval(sm_bus, sender_address):
     time.sleep(1)
-    block = bus.read_i2c_block_data(address, 1)
+    block = sm_bus.read_i2c_block_data(sender_address, 1)
     n = struct.unpack("<l", bytes(block[:4]))[0]
     dvl = n / 100
     while n != -1:
         return dvl
+
+while True:
+    dvl_output = dacval(bus, address)
+    print(str(dvl_output))
