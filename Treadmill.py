c92a6c4eb803b933dc935ccd3cff5f9e09f76e4b
season <season.qiu@yahoo.com>
2021-12-01 15:33:11 -0500

    Treadmill.py: origin version

--- Treadmill.py
+++ Treadmill.py
@@ -8,16 +8,10 @@ bus = smbus.SMBus(1) # "On all recent (since 2014) raspberries the GPIO pin's I2
 address = 0x08
 
 
-def dacval(sm_bus, sender_address):
+def dacval():
     time.sleep(1)
-    block = sm_bus.read_i2c_block_data(sender_address, 1)
+    block = bus.read_i2c_block_data(address, 1)
     n = struct.unpack("<l", bytes(block[:4]))[0]
-    dvl = n
-    # dvl = n / 100
-    # while n != -1:
-    #     return dvl
-    return dvl
-
-while True:
-    dvl_output = dacval(bus, address)
-    print(str(dvl_output))
+    dvl = n / 100
+    while n != -1:
+        return dvl
