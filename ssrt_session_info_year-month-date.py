6e3df3b32e4ac2801cdc476a84585aa270645764
Duy Tran <duy.tran.einsteinmed@gmail.com>
2021-12-02 09:18:15 -0500

    update

--- ssrt_session_info_year-month-date.py
+++ ssrt_session_info_year-month-date.py
@@ -31,12 +31,13 @@ session_info['config']						= 'head_fixed_v1'
 session_info['delay_time']                  = 1  # delay time between astim and vstim (in s)
 session_info['number_of_trials']            = 300  # total number of trials
 session_info['init_length']                 = 0.01  # in seconds
-session_info['lockout_length']              = 0.2  # in seconds
+session_info['lockout_length']              = 0.1  # in seconds
 session_info['vacuum_length']               = 0.5  # in seconds
 session_info['trial_length']                = 3  # length of trial after vstim starts
 session_info['time_for_plotting']           = 0  # based on the readout when running the task
 session_info['reward_available_length']     = session_info['trial_length'] - session_info['lockout_length']
 session_info['reward_size']					= 10  # in microliters
+session_info['reward_duration']             = 0.01
 session_info['iti_length']                  = 3 - session_info['vacuum_length'] - session_info['time_for_plotting']
 #  2.5s for ITI because plotting takes 0.8s-1.2s
 session_info['normal_iti_length']           = 3 - session_info['vacuum_length'] - session_info['time_for_plotting']
--- ssrt_test_behavbox.py
+++ ssrt_test_behavbox.py
@@ -4,7 +4,7 @@ pump = box.Pump()
 
 test_running = True
 while test_running:
-    reward_duration = float(input("What is the reward size: \n"))
+    reward_duration = float(input("What is the reward duration: \n"))
     reward_size = 91  # this is one whole revolution
     # reward_duration = 1
     pump.reward("center", reward_size, reward_duration)  # currently using left pump for the task
