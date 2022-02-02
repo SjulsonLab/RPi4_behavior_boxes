8343852825eccf5db57c1c1ea5f3808675ebe7a4
dtran1125 <duy.tran.einsteinmed@gmail.com>
2022-02-02 12:00:07 -0500

    update

---
+++ test_pump.py
@@ -0,0 +1,14 @@
+import behavbox_DT as box
+
+##### Import the pump object and define parameters for reward method
+pump = box.Pump
+which_pump = "left"  # could be "center" or "right"
+reward_size = 12  # in mL
+reward_duration = 0.01  # fastest
+
+##### Call the reward method to deliver the reward
+pump.reward(which_pump, reward_size, reward_duration)
+
+
+
+
