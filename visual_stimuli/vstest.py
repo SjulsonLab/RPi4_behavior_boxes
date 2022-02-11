94ac6f4909509d2b9f24556d3faa6c4b3a43b1e9
dtran1125 <duy.tran.einsteinmed@gmail.com>
2022-02-11 10:57:31 -0500

    update

--- visual_stimuli/vstest.py
+++ visual_stimuli/vstest.py
@@ -1,10 +1,10 @@
 #Test file from https://github.com/bill-connelly/rpg 
 ################
 import rpg
-options_go = {"duration": 3, "angle": 45, "spac_freq": 0.4, "temp_freq": 2}
+options_go = {"duration": 3, "angle": 45, "spac_freq": 0.3, "temp_freq": 2}
 rpg.build_grating("~/first_grating_go.dat", options_go)
 
-options_nogo = {"duration": 3, "angle": 135, "spac_freq": 0.4, "temp_freq": 2}
+options_nogo = {"duration": 3, "angle": 135, "spac_freq": 0.3, "temp_freq": 2}
 rpg.build_grating("~/first_grating_nogo.dat", options_nogo)
 
 with rpg.Screen() as myscreen:
