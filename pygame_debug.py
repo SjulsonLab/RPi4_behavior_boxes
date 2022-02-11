9e94689b8e64297e8e61c8b7e04f4df855cef899
season <season.qiu@yahoo.com>
2022-02-11 16:08:35 -0500

    pygame_debug.py

--- pygame_debug.py
+++ pygame_debug.py
@@ -5,9 +5,10 @@ try:
 except Exception as error_message:
     print(str(error_message))
 event = pygame.event.poll()
-# KeyDown = 768
-# KeyUp = 769
-while True:
-    time.sleep(0.3)
-    if event.key:
-        print(str(event.key))
+KeyDown = 768
+KeyUp = 769
+    if event:
+        if event.type == KeyDown:
+            print("KeyDown: " + str(event.key))
+        elif event.type == KeyUp:
+            print("KeyUp: " + str(event.key))
