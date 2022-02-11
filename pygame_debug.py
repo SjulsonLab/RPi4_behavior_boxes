c8da4e4055841a8085bd13d8a154bea5998bcc61
season <season.qiu@yahoo.com>
2022-02-11 16:14:24 -0500

    pygame_debug.py

--- pygame_debug.py
+++ pygame_debug.py
@@ -8,7 +8,7 @@ event = pygame.event.poll()
 KeyDown = 768
 KeyUp = 769
 while True:
-    time.sleep(0.3)
+    # time.sleep(0.3)
     if event:
         if event.type == KeyDown:
             print("KeyDown: " + str(event.key))
