1335e4fc41639e99dc04ff4b8ed09e192a78e89a
season <season.qiu@yahoo.com>
2022-02-11 16:04:10 -0500

    pygame_debug.py

--- pygame_debug.py
+++ pygame_debug.py
@@ -2,7 +2,8 @@ import pygame
 import time
 try:
     DISPLAYSURF = pygame.display.set_mode((200, 200))
-
+except Exception as error_message:
+    print(str(error_message))
 event = pygame.event.poll()
 # KeyDown = 768
 # KeyUp = 769
