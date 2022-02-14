098b2aa068ca24199a31568fc74d434cf3c2c12d
season <season.qiu@yahoo.com>
2022-02-13 23:06:14 -0500

    pygame_debug.py and syringe_pump_pygame_debug.py: new script for testing out new features using pygame to prime the syringe pump

--- pygame_debug.py
+++ pygame_debug.py
@@ -1,17 +1,33 @@
 import pygame
 import time
+
+pygame.init()
 try:
     DISPLAYSURF = pygame.display.set_mode((200, 200))
+    pygame.display.set_caption("pygame debugging")
+
 except Exception as error_message:
     print(str(error_message))
-event = pygame.event.poll()
-KeyDown = 768
-KeyUp = 769
-while True:
+# event = pygame.event.poll()
+# KeyDown = 768 # for raspberry pi from linux machine
+# KeyUp = 769 # for raspberry pi from linux machine
+# KeyDown = 771 # for macOS
+# KeyUp = 769 # for macOS
+run = True
+while run:
     # time.sleep(0.3)
-    if event:
-        print(event)
-        if event.type == KeyDown:
-            print("KeyDown: " + str(event.key))
-        elif event.type == KeyUp:
-            print("KeyUp: " + str(event.key))
+    for event in pygame.event.get():
+        # if event.type != 32774:
+        # print(str(event))
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            run = False
+        if event.type == pygame.KEYDOWN:
+            # print("KeyDown: " + str(event.type) + "\n")
+            if event.key == pygame.K_w:
+                print("key W down")
+            # print("KeyDown: " + str(event.key))
+            # print(event)
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_w:
+                print("key W up")
+            # print(event)
---
+++ syringe_pump_pygame_debug.py
@@ -0,0 +1,40 @@
+import pygame
+from behavbox import Pump
+
+try:
+    pump = Pump()
+    pygame.init()
+    DISPLAYSURF = pygame.display.set_mode((200, 200))
+    pygame.display.set_caption("pygame debugging")
+except Exception as error_message:
+    print(str(error_message))
+
+run = True
+reward_size = 20
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            run = False
+        if event.type == pygame.KEYDOWN:
+            # print("KeyDown: " + str(event.type) + "\n")
+            if event.key == pygame.K_q:
+                print("Q down: syringe pump 1 moves")
+                pump.reward("1", reward_size)
+            if event.key == pygame.K_w:
+                print("W down: syringe pump 2 moves")
+                pump.reward("2", reward_size)
+            if event.key == pygame.K_e:
+                print("E down: syringe pump 3 moves")
+                pump.reward("3", reward_size)
+            if event.key == pygame.K_r:
+                print("R down: syringe pump 4 moves")
+                pump.reward("4", reward_size)
+        # elif event.type == pygame.KEYUP:
+        #     if event.key == pygame.K_q:
+        #         print("Q up: syringe pump 1 moves")
+        #     if event.key == pygame.K_w:
+        #         print("W up: syringe pump 2 moves")
+        #     if event.key == pygame.K_e:
+        #         print("E up: syringe pump 3 moves")
+        #     if event.key == pygame.K_r:
+        #         print("R up: syringe pump 4 moves")
