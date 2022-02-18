3ef0187a48097c077622a8a8906e1aeaa4779a65
season <season.qiu@yahoo.com>
2022-02-17 22:55:30 -0500

    task_information_headfixed.py, soyoun_task.py

---
+++ soyoun_task.py
@@ -0,0 +1,406 @@
+# python3: soyoun_task.py
+from transitions import Machine
+from transitions import State
+from transitions.extensions.states import add_state_features, Timeout
+import pysistence, collections
+from icecream import ic
+import logging
+from datetime import datetime
+import os
+from gpiozero import PWMLED, LED, Button
+from colorama import Fore, Style
+import logging.config
+from time import sleep
+import random
+
+logging.config.dictConfig(
+    {
+        "version": 1,
+        "disable_existing_loggers": True,
+    }
+)
+# all modules above this line will have logging disabled
+from task_information_headfixed import task_information
+import behavbox
+
+# adding timing capability to the state machine
+@add_state_features(Timeout)
+class TimedStateMachine(Machine):
+    pass
+
+
+class SoyounTask(object):
+    # Define states. States where the animals is waited to make their decision
+    # session_info = fake_session_info # temporary fill-in
+    def __init__(self, **kwargs):  # name and session_info should be provided as kwargs
+
+        # if no name or session, make fake ones (for testing purposes)
+        if kwargs.get("name", None) is None:
+            self.name = "name"
+            print(
+                Fore.RED
+                + Style.BRIGHT
+                + "Warning: no name supplied; making fake one"
+                + Style.RESET_ALL
+            )
+        else:
+            self.name = kwargs.get("name", None)
+
+        if kwargs.get("session_info", None) is None:
+            print(
+                Fore.RED
+                + Style.BRIGHT
+                + "Warning: no session_info supplied; making fake one"
+                + Style.RESET_ALL
+            )
+            from fake_session_info import fake_session_info
+
+            self.session_info = fake_session_info
+        else:
+            self.session_info = kwargs.get("session_info", None)
+        ic(self.session_info)
+
+        # import task information for the condition setup
+        self.task_information = task_information # when initiated, task_information generates a shuffled deck
+        self.error_repeat = self.task_information['error_repeat']
+        self.error_count_max = self.task_information['error_repeat_max']
+
+        # setup condition for the treadmill signal
+        self.distance_initiation = self.task_information['distance_initiation'] #cm
+        self.distance_short = self.task_information['distance_short']
+        self.distance_max = self.task_information['distance_max']
+
+        # initialize the state machine
+        self.states = [
+            State(name = 'standby',
+                  on_enter = ["enter_standby"],
+                  on_exit = ["exit_standby"]),
+            Timeout(name = "initiate",
+                    on_enter = ["enter_initiate"],
+                    on_exit = ["exit_initiate"],
+                    timeout = self.task_information["initiation_timeout"],
+                    on_timeout = ["start_cue"]),
+            Timeout(name = 'cue_state',
+                    on_enter = ["enter_cue_state"],
+                    on_exit = ["exit_cue_state"],
+                    timeout = self.task_information["cue_timeout"],
+                    on_timeout = ["evaluate_reward"]),
+            Timeout(name = 'reward_available',
+                    on_enter = ["enter_reward_available"],
+                    on_exit = ["exit_reward_available"],
+                    timeout = self.task_information["reward_timeout"],
+                    on_timeout = ["restart"])
+        ]
+        self.transitions = [
+            ['start_trial', 'standby', 'initiate'], # format: ['trigger', 'origin', 'destination']
+            ['start_cue', 'initiate','cue_state'],
+            ['evaluate_reward', 'cue_state', 'reward_available'],
+            ['restart', ['initiate', 'cue_state', 'reward_available'], 'standby']
+        ]
+
+        self.machine = TimedStateMachine(
+            model=self,
+            states=self.states,
+            transitions=self.transitions,
+            initial='standby'
+        )
+        self.trial_running = True
+        self.restart_flag = False
+
+        self.error_count = 0
+        self.card_count = 0
+        self.deck = self.task_information["deck"]
+        self.current_card = None
+
+        # initialize behavior box
+        self.box = behavbox.BehavBox(self.session_info)
+        self.pump = behavbox.Pump()
+        self.treadmill = self.box.treadmill
+        self.distance_initiation = task_information['treadmill_setup']['distance_initiation']
+        self.distance_short = task_information['treadmill_setup']['distance_short']
+
+    ########################################################################
+    # functions called when state transitions occur
+    ########################################################################
+    def run(self):
+
+        if self.box.event_list:
+            event_name = self.box.event_list.popleft()
+        else:
+            event_name = ""
+
+        if self.state == "reward_available":
+            # first detect the lick signal:
+            side_choice = self.task_information['choice'][self.current_card[1]]
+            # question: do we want entry mark as lick?
+            if event_name == "left_IR_entry": side_correct = 'left'
+            elif event_name == "right_IR_entry": side_correct = 'right'
+            if side_choice == side_correct:
+                self.pump.reward(side_correct, self.task_information["reward_size"])
+
+        # look for keystrokes
+        self.box.check_keybd()
+
+
+    def enter_standby(self):
+        # check error_repeat
+        if self.card_count > self.task_information["total_block_length"]:
+            self.trial_running = False # terminate the state machine, end the session
+        elif self.error_repeat and (self.error_count_max < self.error_count_max):
+            self.restart_flag = True
+        else:
+            self.restart_flag = False
+            pass
+
+    def exit_standby(self):
+        # check the flag to see whether to shuffle or keep the original card
+        if self.restart_flag:
+            self.error_count += 1
+        else:
+            self.current_card = self.deck[self.card_count]
+            self.card_count += 1
+
+    def enter_cue_state(self, wait_time):
+        # turn on the cue according to the current card
+        self.check_cue(self.current_card[0])
+        # wait for treadmill signal and process the treadmill signal
+        distance_start = self.treadmill.distance()
+        sleep(wait_time)
+        distance_end = self.treadmill.distance()
+        distance_pass = self.check_distance(distance_end - distance_start, self.distance_initiation)
+        if not distance_pass: self.restart_flag = True
+        else: self.restart_flag = False
+
+    def exit_cue_state(self):
+        if self.restart_flag:
+            self.error_count += 1
+            self.restart()
+
+
+    def enter_reward_available(self):
+        pass
+
+    def exit_reward_available(self):
+        self.restart()
+
+    def check_cue(self, cue):
+        if cue == 'sound':
+            self.sound1.on() # could be modify according to specific sound cue
+        elif cue == 'LED':
+            self.cueLED1.on()
+        else:
+            print("Not valid cue: " + cue)
+
+    def check_distance(self, distance_now, distance_required):
+        if distance_now >= distance_required:
+            pass
+        else:
+            return False
+        return True
+
+    # def check_lick(self, side_index):
+    #     # first detect the lick signal:
+    #     side = self.task_information['choice'][side_index]
+    #     event_name = self.box.event_list.popleft()
+    #     if side == 'right':
+    #
+    #     elif side == 'left':
+    #
+    #     else:
+    #         print(str(side) + " is not allowed")
+    #         self.restart_flag == False
+    #
+    # def check_reward(self, index):
+    #     reward = self.task_information['reward'][index]
+    #     reward_size = self.task_information['reward_size'][reward]
+    #     self.pump.reward()
+
+    # def initiate(self):
+    #
+    #
+    # def short_distance(self):
+    #     """
+    #     only if animal walked equal or less than x2
+    #     else throw error + callback
+    #     """
+    #     print("the animal walked a short distance(x2 cm)")
+    #     print("Cue off")
+    #
+    # def long_distance(self):
+    #     """
+    #     only if animal walked more than x2 distance
+    #     else transition to short_distance and throw error callback
+    #     """
+    #     print("the animal walked a long distance(>x2 cm)")
+    #     print('Cue off')
+    #
+    # def lick_detected(self):
+    #     print("entering reward_delivery")
+    #
+    # def run(self):
+    #     """
+    #     if enter from state_short:
+    #         if animal licked left:
+    #             pump deliver higher reward
+    #         if animal licked right:
+    #             pump deliver lower reward
+    #         else(not licked within required time):
+    #             time out
+    #     if enter from state_long:
+    #         if animal licked left:
+    #             pump deliver lower reward
+    #         if animal licked right:
+    #             pump deliver higher reward
+    #         else:
+    #             time out
+    #     """
+    #     if self.box.event_list:
+    #         event_name = self.box.event_list.popleft()
+    #     else:
+    #         event_name = ""
+    #
+    #     if self.state == "standby":
+    #         distance_start = self.treadmill.running_speed
+    #         while True:
+    #             distance_buffer = self.treadmill.running_speed - distance_start
+    #             if distance_buffer > self.dist_init:
+    #                 break
+    #     if self.state == "cue":
+    #         if self.session_info['initial_state'] == 'sound_long':
+    #             # sound on: long distance = large reward; short distance = smalle reward
+    #             if event_name == "left_IR_entry":
+    #                 ic("large reward")
+    #                 # question: which pump gives the larger reward, which is not?
+    #                 self.pump.reward("left", self.session_info["reward_size"])
+    #             if event_name == "right_IR_entry":
+    #                 ic("small reward")
+    #                 self.pump_reward("right", self.session_info["reward_size"])
+    #         if self.session_info['initial_state'] == 'sound_short':
+    #             if event_name == "left_IR_entry":
+    #                 ic("small reward")
+    #                 # question: which pump gives the larger reward, which is not?
+    #                 self.pump.reward("right", self.session_info["reward_size"])
+    #             if event_name == "right_IR_entry":
+    #                 ic("large reward")
+    #                 self.pump_reward("left", self.session_info["reward_size"])
+
+
+"""
+consecutive control
+    same condition can not be repeated for more than 3 trials
+correction trial
+    repeat the mistaken trial the animal until they are correct
+
+
+block = 1
+    force choice condition (LED - large, sound - small)
+        LED - short right, reward = large
+        LED - long left, reward = large
+        
+        sound - short left, reward = small
+        sound - long right, reward = small
+        
+        if free choice condition
+            LED, sound - short
+                if choose right: reward = large
+                if choose left: reward = small
+            LED, sound - long
+                if choose left: reward = large
+                if choose right: reward = small
+                
+                
+block = 2
+    force choice condition (LED - small, sound - large)
+        LED - short right, reward = small
+        LED - long left, reward = small
+        
+        sound - short left, reward = large
+        sound - long right, reward = large
+        
+        if free choice condition
+            LED, sound - short
+                if choose right: reward = small
+                if choose left: reward = large
+            LED, sound - long
+                if choose left: reward = small
+                if choose right: reward = large
+            
+"""
+
+"""
+State machine structure
+State machine states:
+    "standby" - the beginning of each trial, initiate the psudo-random trial content (shuffle)
+    "cue_state" - initiate by the x1 distance and turn on the appropriate cue according to the pre-configured trial context
+    "cue_off" - triggered by the satisfaction of treadmill signal x1, and turn off the cue. detect x2 or x3 from treadmill signal
+    "detect_reward" - detect lick spot signal and deliver corresponding reward or punishment
+    
+State machine transition:
+    enter_standby
+        if previous trial wrong + error_repeat == True + error_repeat_count <= error_repeat_max:
+            don't change the condition parameters
+        else:
+            error_repeat_count = 0
+            psudo-shuffle - in fact extract trial information from pre-set psudo-randomize deck
+                shuffle += 1
+                trial_deck[shuffle] # a dictionary contains condition parameters
+    exit_standyby
+        wait for the treadmill signal fullfill x1 amount of distance
+        no_respond more than a set amount of time:
+            error_repeat_count += 1
+            timeout(_seconds)
+            enter_standby
+        else:
+            pass
+    enter_cue_state
+        pass
+    exit_cue_state
+        play cue
+        pass
+    enter_cue_off
+        wait for the treadmill signal fullfill x2 or x3 amount of distance (according to the specific trial requirement)
+        if fullfilled, turn off the cue or return to the beginning of the trial if longer than delay or detect wrong
+        response
+    exit_cue_off
+        pass
+    enter_detect_reward
+        detect lick spot
+    exit_detect_reward
+        if wrong:
+            if allow_switch == False:
+                error_repeat_count += 1
+                enter_standby
+        if right:
+            deliver the appropriate reward
+            error_repeat_count = 0
+            enter_standby
+    
+"""
+
+
+"""
+# within the run_file
+def shuffle_conditions:
+    create a list of conditon
+    condition index(row_number);        cue;        state;      lick_side;
+    create a list of cue
+        block_1_forced_choice: {
+            'cue': [TRUE, TRUE, FALSE, FALSE], # TRUE: LED, FALSE: sound
+            'state': [FALSE, TRUE, FALSE, TRUE], # TRUE: long, FALSE: short
+            'lick_side': [FALSE, TRUE, TRUE, FALSE], # TRUE: left, FALSE: right
+            'reward_size': [TRUE, TRUE, FALSE, FALSE] # TRUE: large, FALSE: small
+        }
+        
+        block_2_forced_choice = block_1_forced_choice
+        block_2_forced_choice['reward_size'] = not block_1_forced_choice['reward_size']
+        
+        block_1_free_choice: {
+            'cue': None
+            'state': [FALSE, FALSE, TRUE, TRUE], # TRUE: long, FALSE: short
+            'lick_side': [FALSE, TRUE, FALSE, TRUE], # TRUE: left, FALSE: right
+            'reward_size': [TRUE, FALSE, FALSE, TRUE] # TRUE: large, FALSE: small
+        }
+        
+        block_2_free_choice = block_1_free_choice
+        block_2_free_choice['reward_size'] = not block_1_free_choice['reward_size']
+"""
--- task_information_headfixed.py
+++ task_information_headfixed.py
@@ -1,16 +1,14 @@
 # Python3: task_information_2022_02_14.py
 import random
-from datetime import datetime
 import pysistence, collections
-import socket
 
 task_information = collections.OrderedDict()
 task_information['experiment_setup'] = 'headfixed'
 task_information['treadmill_setup'] = {'present': True}
 
 if task_information['treadmill_setup']['present']:
-    task_information['treadmill_setup']['distance_initiation'] = 3 #cm
-    task_information['treadmill_setup']['distance_short'] = 5 #cm
+    task_information['treadmill_setup']['distance_initiation'] = 3  # cm
+    task_information['treadmill_setup']['distance_short'] = 5  # cm
     task_information['treadmill_setup']['distance_max'] = None
 else:
     task_information['treadmill_setup'] = None
@@ -21,58 +19,69 @@ if task_information['error_repeat']:
 
 # condition setup
 task_information['cue'] = ['sound', 'LED']
-task_information['state'] = ['distance_short', 'distance_long'] # treadmill distance
-task_information['choice'] = ['right', 'left'] # lick port
-task_information['reward'] = ['small', 'large'] # reward size
+task_information['state'] = ['distance_short', 'distance_long']  # treadmill distance
+task_information['choice'] = ['right', 'left']  # lick port
+task_information['reward'] = ['small', 'large']  # reward size
+task_information['reward_size'] = {'small': 10, 'large': 20}
+
+# define timeout during each condition
+task_information['initiation_timeout'] = 3 #s
+task_information['cue_timeout'] = 3
+task_information['reward_timeout'] = 3
 
 # block setup
-task_information['block']['forced_1'] = [
-                                (1, 0, 0, 1), # forced choice block 1
-                                (1, 1, 1, 1), # 0, 1 index for corresponding condition setup
-                                (0, 0, 1, 0), # column 0 is cue, 1 is state, 2 is choice, 3 is reward_amount
-                                (0, 1, 0, 0)] # each row is a combination of the condition parameter for block 1
+task_information['block'] = {}
+task_information['block'][1] = [
+    # block_1 forced component
+    (1, 0, 0, 1),  # forced choice block 1
+    (1, 1, 1, 1),  # 0, 1 index for corresponding condition setup
+    (0, 0, 1, 0),  # column 0 is cue, 1 is state, 2 is choice, 3 is reward_amount
+    (0, 1, 0, 0),  # each row is a combination of the condition parameter for block 1
+    # block_1 free component
+    (2, 0, 0, 1),  # free choice block 1
+    (2, 0, 1, 0),  # 0, 1 index for corresponding condition setup, 2 in column 0
+    (2, 1, 0, 0),  # means both cues are on
+    (2, 1, 0, 1)]  # each row is a combination of the condition parameter for block 1
 
-task_information['block']['forced_2'] = [
-                                (1, 0, 0, 0), # forced choice block 2
-                                (1, 1, 1, 0), # 0, 1 index for corresponding condition setup
-                                (0, 0, 1, 1), # column 0 is cue, 1 is state, 2 is choice, 3 is reward_amount
-                                (0, 1, 0, 1)] # each row is a combination of the condition parameter for block 1
-
-task_information['block']['free_1'] = [
-                                (None, 0, 0, 1), # free choice block 1
-                                (None, 0, 1, 0), # 0, 1 index for corresponding condition setup, None in column 0
-                                (None, 1, 0, 0), # means both cues are on
-                                (None, 1, 0, 1)] # each row is a combination of the condition parameter for block 1
-
-task_information['block']['free_2'] = [
-                                (None, 0, 0, 0), # free choice block 2
-                                (None, 0, 1, 1), # 0, 1 index for corresponding condition setup, None in column 0
-                                (None, 1, 0, 1), # means both cues are on
-                                (None, 1, 0, 0)] # each row is a combination of the condition parameter for block 1
+task_information['block'][2] = [
+    # block_2 forced component
+    (1, 0, 0, 0),  # forced choice block 2
+    (1, 1, 1, 0),  # 0, 1 index for corresponding condition setup
+    (0, 0, 1, 1),  # column 0 is cue, 1 is state, 2 is choice, 3 is reward_amount
+    (0, 1, 0, 1),  # each row is a combination of the condition parameter for block 1
+    # block_2 free component
+    (2, 0, 0, 0),  # free choice block 2
+    (2, 0, 1, 1),  # 0, 1 index for corresponding condition setup, None in column 0
+    (2, 1, 0, 1),  # means both cues are on
+    (2, 1, 0, 0)]  # each row is a combination of the condition parameter for block 1
 
 # now shuffle and make a deck for this session
 # shuffle requirement
-initial_condition = 0 # 0,1,2,3 row number
+
+# allowing user defined initial_block and initial setup for conditions?
+task_information["initial_block"] = 1
+
+# allowing consecutive repeated trial?
 consecutive_control = True
 if consecutive_control:
     consecutive_max = 3
-duration_1_forced = 30
-duration_1_free = 30
-duration_2_forced = 30
-duration_2_free = 30
 
-# task_information['block_1_forced_deck'] = []
-row_buffer = -1
-consecutive_count = 0
+block_length_list = {
+    1: 10,
+    2: 10
+}
+task_information['block_name_list'] = [block_length_list.keys()]
 
-def generate_deck(duration):
+def generate_deck(duration, consecutive_control, consecutive_max):
+    consecutive_count = 0
+    row_buffer = -1
     deck_list = []
     for iteration in range(duration):
-        row_index = random.randrange(0, 4)
+        row_index = random.randrange(0, 8)
         while True:
             if row_index == row_buffer and consecutive_control:
                 if consecutive_count >= consecutive_max:
-                    row_index = random.randrange(0,4)
+                    row_index = random.randrange(0, 8)
                 else:
                     break
             else:
@@ -81,8 +90,27 @@ def generate_deck(duration):
         deck_list.append(row_index)
     return deck_list
 
-task_information["deck_list"] = {}
-task_information["deck_list"]["block_1_forced"] = generate_deck(duration_1_forced)
-task_information["deck_list"]["block_2_forced"] = generate_deck(duration_2_forced)
-task_information["deck_list"]["block_1_free"] = generate_deck(duration_1_free)
-task_information["deck_list"]["block_2_free"] = generate_deck(duration_2_free)
+block_length = 10
+block_list = []
+initial_block = 1  # False if no initial block
+
+
+def shuffle(block_length, block_list, initial_block):
+    if initial_block:
+        block_list.append(initial_block)
+        block_list.extend([random.randrange(1, 3) for i in range(block_length - 1)])
+    else:
+        block_list = [random.randrange(1, 3) for i in range(block_length)]
+
+    deck = []
+    for block in block_list:
+        deck_list_buffer = generate_deck(block_length_list[1], consecutive_control, consecutive_max)
+        current_deck = []
+        block_map = task_information['block'][block]
+        for row_index in deck_list_buffer:
+            current_deck.append(block_map[row_index])
+        deck.extend(current_deck)
+    return deck
+
+
+task_information["deck"] = shuffle(block_length, block_list, initial_block)
