12e5f4edf6f2580eb1b12a2b3991e922f9724328
seasonqiu <seaosn.qiu@yahoo.com>
2022-03-17 12:11:57 -0400

    task_information_headfixed: correct the phase 1 row_index

--- task_information_headfixed.py
+++ task_information_headfixed.py
@@ -5,21 +5,21 @@ import random
 class TaskInformation(object):
     def __init__(self, **kwargs):
         self.name = "model_based_reinforcement_learning_task"
-        self.block = {1: [
-            # block_1 forced component
-            ('LED', 'distance_short', 'right', 'large'),  # forced choice block 1
-            ('LED', 'distance_long', 'left', 'large'),  # 0, 1 index for corresponding condition setup
-            ('sound', 'distance_short', 'left', 'small'),
-            # column 0 is cue, 1 is state, 2 is choice, 3 is reward_amount
-            ('sound', 'distance_long', 'right', 'small'),
-            # each row is a combination of the condition parameter for block 1
-            # block_1 free component
-            ('sound+LED', 'distance_short', 'right', 'large'),  # free choice block 1
-            ('sound+LED', 'distance_short', 'left', 'small'),
-            # 0, 1 index for corresponding condition setup, 2 in column 0
-            ('sound+LED', 'distance_long', 'right', 'small'),  # means both cues are on
-            ('sound+LED', 'distance_long', 'right',
-             'large')],
+        self.block = {
+            1: [
+                # block_1 forced component
+                ('LED', 'distance_short', 'right', 'large'),  # forced choice block 1
+                ('LED', 'distance_long', 'left', 'large'),  # 0, 1 index for corresponding condition setup
+                ('sound', 'distance_short', 'left', 'small'),
+                # column 0 is cue, 1 is state, 2 is choice, 3 is reward_amount
+                ('sound', 'distance_long', 'right', 'small'),
+                # each row is a combination of the condition parameter for block 1
+                # block_1 free component
+                ('sound+LED', 'distance_short', 'right', 'large'),  # free choice block 1
+                ('sound+LED', 'distance_short', 'left', 'small'),
+                # 0, 1 index for corresponding condition setup, 2 in column 0
+                ('sound+LED', 'distance_long', 'right', 'small'),  # means both cues are on
+                ('sound+LED', 'distance_long', 'right', 'large')],
             2: [
                 # block_2 forced component
                 ('LED', 'distance_short', 'right', 'small'),  # forced choice block 2
@@ -33,16 +33,16 @@ class TaskInformation(object):
                 ('sound+LED', 'distance_short', 'left', 'large'),
                 # 0, 1 index for corresponding condition setup, None in column 0
                 ('sound+LED', 'distance_long', 'right', 'large'),  # means both cues are on
-                ('sound+LED', 'distance_long', 'right',
-                 'small')]}
+                ('sound+LED', 'distance_long', 'right', 'small')]}
 
     def draw_card(self, block_key, phase):
         if phase == 1:
-            row_end = 2
+            row_end = 3
         else:
             row_end = 8
         block_map = self.block[block_key]
-        row_index = random.randint(1, row_end)
+        row_index = random.randint(0, row_end)
+        # print(str(row_index))
         card = block_map[row_index]
 
         return card
