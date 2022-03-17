0a07ce596b89ded0c2000266af883eefc72b5cf0
seasonqiu <seaosn.qiu@yahoo.com>
2022-03-17 18:41:13 -0400

    testing the soundboard

--- headfixed_task.py
+++ headfixed_task.py
@@ -118,11 +118,14 @@ class HeadfixedTask(object):
         self.distance_initiation = self.session_info['treadmill_setup']['distance_initiation']
         self.distance_buffer = self.treadmill.distance_cm
         self.distance_diff = 0
+        self.sound_on = False
 
     ########################################################################
     # functions called when state transitions occur
     ########################################################################
     def run(self):
+        if self.sound_on == True:
+            self.box.sound1.blink(0.01, 1, 2)
         if self.box.event_list:
             event_name = self.box.event_list.popleft()
         else:
@@ -136,8 +139,6 @@ class HeadfixedTask(object):
             else:
                 self.error_count += 1
         elif self.state == "cue_state":
-            if self.current_card[0] == 'sound' or 'sound+LED':
-                self.box.sound1.blink(0.01, 1, 2)
             self.distance_diff = self.treadmill.distance_cm - self.distance_buffer
             distance_condition = self.current_card[1]
             distance_required = self.session_info['treadmill_setup'][distance_condition]
@@ -220,26 +221,26 @@ class HeadfixedTask(object):
 
     def check_cue(self, cue):
         if cue == 'sound':
-            self.box.sound1.blink(0.1,0.1,5)  # could be modify according to specific sound cue
             logging.info(str(time.time()) + ", " + str(self.trial_number) + ", cue sound1 on")
+            self.sound_on = True
         elif cue == 'LED':
             self.box.cueLED1.on()
             logging.info(str(time.time()) + ", " + str(self.trial_number) + ", cueLED1 on")
         else:
             self.box.cueLED1.on()
-            self.box.sound1.blink(0.1, 0.1, 5)
+            self.sound_on = True
             logging.info(str(time.time()) + ", " + str(self.trial_number) + ", sound1 + cueLED1 on (free choice)")
 
     def cue_off(self, cue):
         if cue == 'sound':
-            # self.box.sound1.off()  # could be modify according to specific sound cue
+            self.sound_on = False
             logging.info(str(time.time()) + ", " + str(self.trial_number) + ", cue sound1 off")
             pass
         elif cue == 'LED':
             self.box.cueLED1.off()
             logging.info(str(time.time()) + ", " + str(self.trial_number) + ", cueLED1 off")
         else:
-            # self.box.sound1.off()
+            self.sound_on = False
             self.box.cueLED1.off()
             logging.info(str(time.time()) + ", " + str(self.trial_number) + ", sound1 + cueLED1 off (free choice)")
 
