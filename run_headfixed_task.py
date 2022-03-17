05302a544f5af124f8ef3fb62b62052bc4eb7380
seasonqiu <seaosn.qiu@yahoo.com>
2022-03-17 11:48:51 -0400

    headfixed_new description for the task and run file

--- run_headfixed_task.py
+++ run_headfixed_task.py
@@ -120,11 +120,12 @@ try:
             while first_card or (session_info["error_repeat"] and task.error_count < session_info["error_max"]):
                 task.trial_number += 1
                 first_card = False
+                print("*******************************\n")
                 task.current_card = task_information.draw_card(block_number, session_info['phase'])
                 logging.info(str(time.time()) + ", " + str(task.trial_number) + ", current_card: " + str(task.current_card))
-                print("****************************\n" + "Block " + str(block_number) +
-                      "Current card condition: \n" +
-                      "****************************\n" +
+                print("Block " + str(block_number) +
+                      " - Current card condition: \n" +
+                      "*******************************\n" +
                       "*Cue: " + str(task.current_card[0]) + "\n" +
                       "*State: " + str(task.current_card[1]) + "\n" +
                       "*Choice: " + str(task.current_card[2]) + "\n" +
