7fc0ffdbc46af07c371a955bbf7559987101560b
season <season.qiu@yahoo.com>
2023-02-01 13:57:46 -0500

    behavbox.py air_puff pin number change

--- behavbox.py
+++ behavbox.py
@@ -534,7 +534,7 @@ class Pump(object):
             self.reward_list.append(("pump4_reward", reward_size))
             logging.info(";" + str(time.time()) + ";[reward];pump4_reward_" + str(reward_size))
         elif which_pump == "air_puff":
-            self.pump4.blink(duration, 0.1, 1)
+            self.pump_air.blink(duration, 0.1, 1)
             self.reward_list.append(("pump4_reward", reward_size))
             logging.info(";" + str(time.time()) + ";[reward];pump4_reward_" + str(reward_size))
         elif which_pump == "vacuum":
