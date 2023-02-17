6e764c396962d412d92151c1b84dfd4833b261fb
Soyoun Kim <soyounk@gmail.com>
2023-02-17 16:27:47 -0500

    Update session_info_headfixed2FC.py

--- session_info_headfixed2FC.py
+++ session_info_headfixed2FC.py
@@ -1,4 +1,3 @@
-
 # put all of your mouse and session info in here
 
 from datetime import datetime
@@ -8,43 +7,41 @@ import socket
 
 # defining immutable mouse dict (once defined for a mouse, NEVER EDIT IT)
 mouse_info = pysistence.make_dict({'mouse_name': 'test',
-                 'fake_field': 'fake_info',
-                 })
+                                   'fake_field': 'fake_info',
+                                   })
 
 # Information for this session (the user should edit this each session)
-session_info                              	= collections.OrderedDict()
-session_info['mouse_info']					= mouse_info
-session_info['mouse_name']                 	= mouse_info['mouse_name']
+session_info = collections.OrderedDict()
+session_info['mouse_info'] = mouse_info
+session_info['mouse_name'] = mouse_info['mouse_name']
 
-session_info['basedir']					  	= '/home/pi/buffer'
-session_info['external_storage']            = '/mnt/hd'
-session_info['flipper_filename']            = '/home/pi/buffer/flipper_timestamp'
+session_info['basedir'] = '/home/pi/buffer'
+session_info['external_storage'] = '/mnt/hd'
+session_info['flipper_filename'] = '/home/pi/buffer/flipper_timestamp'
 # for actual data save to this dir:
 # session_info['basedir']					  	= '/home/pi/video'
-session_info['weight']                	    = 0.0
-session_info['manual_date']					= '202x-xx-xx'
-session_info['box_name']             		= socket.gethostname()
-session_info['block_number']                = 1 # 1 (left large) or 2 (right large) which block starts
+session_info['weight'] = 0.0
+session_info['manual_date'] = '202x-xx-xx'
+session_info['box_name'] = socket.gethostname()
+session_info['block_number'] = 1  # 1 (left large) or 2 (right large) which block starts
 
-
-session_info['config']						= 'headfixed2FC'
+session_info['config'] = 'headfixed2FC'
 
 # behavior parameters
-session_info['timeout_length']              = 5  # in seconds
-session_info['reward_size']					= 10  # in microliters
-session_info["lick_threshold"]              = 1
+session_info['timeout_length'] = 5  # in seconds
+session_info['reward_size'] = 10  # in microliters
+session_info["lick_threshold"] = 1
 # visual stimulus
-session_info["visual_stimulus"]             = False
+session_info["visual_stimulus"] = False
 
-session_info['config']	                    = 'headfixed2FC'
-session_info['treadmill_setup']             = {}
-session_info['treadmill']                    = True
-session_info['phase']                   	= 'forced_choice'
-session_info['treadmill_setup']['distance_cue'] = 5
+session_info['config'] = 'headfixed2FC'
+session_info['treadmill_setup'] = {}
+session_info['treadmill'] = True
+session_info['phase'] = 'forced_choice'
 
 if session_info['treadmill']:
+    session_info['treadmill_setup']['distance_cue'] = 5  # cm
     session_info['treadmill_setup']['distance_initiation'] = 10  # cm
-    #session_info['treadmill_setup']['distance_cue'] = 5  # cm
 else:
     session_info['treadmill_setup'] = None
 
@@ -54,7 +51,7 @@ if session_info['error_repeat']:
 
 # condition setup
 session_info['cue'] = ['LED_L', 'LED_R', 'all']
-#session_info['state'] = ['block1', 'block2']  #
+# session_info['state'] = ['block1', 'block2']  #
 session_info['choice'] = ['right', 'left']  # lick port
 session_info['reward'] = ['small', 'large']  # reward size
 session_info['reward_size'] = {'small': 5, 'large': 10}
