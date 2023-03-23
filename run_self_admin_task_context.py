5ddd1149b14e439758a62ff302fccb933612ec55
mitchfarrell <mrfarrel@uci.edu>
2023-03-23 18:07:07 -0400

    update 3-23-23

--- run_self_admin_task_context.py
+++ run_self_admin_task_context.py
@@ -113,6 +113,7 @@ try:
             print("Times up, finishing up")
             break
         logging.info(";" + str(time.time()) + ";[transition];start_trial()")
+        task.switch_to_reward_available()
         while task.trial_running: #trial running in both standby and reward_available
             task.run()  # run command trigger additional functions outside of the state machine
         print("error_count: " + str(task.error_count))
