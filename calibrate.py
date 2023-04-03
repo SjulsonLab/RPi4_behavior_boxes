cd78cae1efea122435c4d1744fd8aaec8c4ef353
season <season.qiu@yahoo.com>
2023-04-03 16:27:11 -0400

    naming error

--- calibrate.py
+++ calibrate.py
@@ -68,17 +68,17 @@ while True:
     # deliver the water using the pump object
     # pump.reward(pump_number, on_duration, off_duration, pulse_time)
     if pump_number == "1":
-        LED(19).blink(on_duration, off_duration, iteration)
-        print("pump1, " + str(on_duration) + str(off_duration) + str(iteration))
+        LED(19).blink(on_duration, off_duration, pulse_time)
+        print("pump1, " + str(on_duration) + str(off_duration) + str(pulse_time))
     elif pump_number == "2":
-        LED(20).blink(on_duration, off_duration, iteration)
-        print("pump2, " + str(on_duration) + str(off_duration) + str(iteration))
+        LED(20).blink(on_duration, off_duration, pulse_time)
+        print("pump2, " + str(on_duration) + str(off_duration) + str(pulse_time))
     elif pump_number == "3":
-        LED(21).blink(on_duration, off_duration, iteration)
-        print("pump3, " + str(on_duration) + str(off_duration) + str(iteration))
+        LED(21).blink(on_duration, off_duration, pulse_time)
+        print("pump3, " + str(on_duration) + str(off_duration) + str(pulse_time))
     elif pump_number == "4":
-        LED(7).blink(on_duration, off_duration, iteration)
-        print("pump4, " + str(on_duration) + str(off_duration) + str(iteration))
+        LED(7).blink(on_duration, off_duration, pulse_time)
+        print("pump4, " + str(on_duration) + str(off_duration) + str(pulse_time))
     time.sleep((on_duration+off_duration)*pulse_time + 0.1)
     print("Please go weight the container with the liquid!\n")
     weight_total = float(input("weight_total: "))
