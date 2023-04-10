0b43a6f9e6a62d00bd67588d279a7cff3ce1ba81
mitchfarrell <mrfarrel@uci.edu>
2023-04-10 18:03:38 -0400

    updated run left_actions and right_actions

--- run_self_admin_task_right_actions.py
+++ run_self_admin_task_right_actions.py
@@ -89,7 +89,7 @@ try:
         ]
     )
 
-    task = SelfAdminTask(name="self_admin_task", session_info=session_info)
+    task = SelfAdminTaskRightActions(name="self_admin_task_right_actions", session_info=session_info)
 
     # start session
     task.start_session()
