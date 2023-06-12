19d932abba1e36d51a9ba2936ef00c26da22abcb
mitchfarrell <mrfarrel@uci.edu>
2023-06-12 16:20:43 -0400

    update LED11

--- behavbox.py
+++ behavbox.py
@@ -100,7 +100,7 @@ class BehavBox(object):
         ###############################################################################################
         # self.DIO3 = LED(9)  # reserved for vacuum function
         # self.DIO4 = LED(10)
-        self.DIO5 = LED(11)
+        # self.DIO5 = LED(11)
         # there is a DIO6, but that is the same pin as the camera strobe
 
         ###############################################################################################
