7d778a82d335ab9848df2f699b4b8d8b265356ff
mitchfarrell <mrfarrel@uci.edu>
2023-06-12 16:20:29 -0400

    update LED11

--- remi_self_admin_lick_task.py
+++ remi_self_admin_lick_task.py
@@ -154,7 +154,7 @@ class remi_self_admin_lick_task(object):
         self.lick_count = 0
         self.side_mice_buffer = None
         self.LED_blink = False
-        self.syringe_pump = LED(10)
+        self.syringe_pump = LED(11)
         self.reward_list = []
 
         try:
