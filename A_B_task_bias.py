a63a01bbb6a226e7a41bd21980f99bfc0f78b575
mitchfarrell <mrfarrel@uci.edu>
2023-06-27 14:43:06 -0400

    A_B_task_bias updates

--- A_B_task_bias.py
+++ A_B_task_bias.py
@@ -332,9 +332,9 @@ class A_B_task_bias(object):
 
     def switch_to_ContextA_B(self):
         self.trial_counter += 1
-        if self.full_task_names_and_times[self.trial_counter][0] == 'ContextB' and self.right_entry_bool == True:
+        if self.full_task_names_and_times[self.trial_counter][0] == 'ContextB' and (self.right_entry_bool == True or self.left_entry_bool == True):
             self.switch_to_ContextB()
-        elif self.full_task_names_and_times[self.trial_counter][0] == 'ContextA' and self.left_entry_bool == True:
+        elif self.full_task_names_and_times[self.trial_counter][0] == 'ContextA' and (self.right_entry_bool == True or self.left_entry_bool == True):
             self.switch_to_ContextA()
         elif self.full_task_names_and_times[self.trial_counter][0] == 'ContextB' and self.right_entry_bool == False:
             self.switch_to_ContextA()
