0b161be73d6e29dee05b4f9b78e8bc513dd9f032
mitchfarrell <mrfarrel@uci.edu>
2023-09-05 14:50:02 -0400

    A_B_task_xreward_switch updates

--- A_B_task_xreward_switch.py
+++ A_B_task_xreward_switch.py
@@ -358,8 +358,9 @@ class A_B_task_xreward_switch(object):
         logging.info(";" + str(time.time()) + ";[transition];enter_ContextA")
         self.box.sound1.blink(0.1, 0.1)
         self.trial_running = True
-        while self.state == 'ContextA': #think about best ways to implement this...
+        while self.state == 'ContextA':
             self.box.visualstim.show_grating(list(self.box.visualstim.gratings)[0],0)
+            time.sleep(1)
 
     def exit_ContextA(self):
         logging.info(";" + str(time.time()) + ";[transition];exit_ContextA")
@@ -375,6 +376,7 @@ class A_B_task_xreward_switch(object):
         self.trial_running = True
         while self.state == 'ContextB':
             self.box.visualstim.show_grating(list(self.box.visualstim.gratings)[5],0)
+            time.sleep(1)
 
     def exit_ContextB(self):
         logging.info(";" + str(time.time()) + ";[transition];exit_ContextB")
