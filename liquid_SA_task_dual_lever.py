7a3e850e9e6674e600e3076f7985420db46b204b
mitchfarrell <mrfarrel@uci.edu>
2023-12-05 13:43:19 -0500

    liquid_SA_task_dual_lever

--- liquid_SA_task_dual_lever.py
+++ liquid_SA_task_dual_lever.py
@@ -172,7 +172,7 @@ class LiquidSATaskDualLever(object):
                 self.event_name = self.box.event_list.popleft()
             else:
                 self.event_name = ''
-            if self.event_name == 'reserved_rx1_pressed':
+            if self.event_name == 'reserved_rx1':
                 entry_time_temp = time.time()
                 entry_dt = entry_time_temp - self.entry_time
                 if entry_dt >= self.entry_interval:
