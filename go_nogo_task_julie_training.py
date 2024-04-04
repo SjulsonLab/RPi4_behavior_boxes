3c8219b3c1d81db6a516a80bf9e3954ac81aba47
jjouvion <165422261+jjouvion@users.noreply.github.com>
2024-04-04 15:51:16 -0400

    Update go_nogo_task_julie_training.py

--- go_nogo_task_julie_training.py
+++ go_nogo_task_julie_training.py
@@ -330,9 +330,13 @@ class go_nogo_phase1(object):
         logging.info(str(time.time()) + ", entering temp2")
         self.trial_outcome = 4  # FA!!!
         logging.info(str(time.time()) + ", FA!!!")
+        self.pump.pump2.on()
+        logging.info(str(time.time()) + ", airpuff ON!")
 
     def exit_temp2(self):
         logging.info(str(time.time()) + ", exiting temp2")
+        self.pump.pump2.off()
+        logging.info(str(time.time()) + ", airpuff OFF!")
 
     def enter_vacuum(self):
         logging.info(str(time.time()) + ", entering vacuum")
