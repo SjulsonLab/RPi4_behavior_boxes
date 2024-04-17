a777469d6d9877044cc9f4a51878af0148a07ee5
Matthew Chin <35109891+mattchin35@users.noreply.github.com>
2024-04-17 19:47:32 -0400

    updates for testing visuals

--- essential/visual_stimuli/vstest.py
+++ essential/visual_stimuli/vstest.py
@@ -3,6 +3,7 @@
 import rpg
 import time
 from pathlib import Path
+from icecream import ic
 
 gratings_dir = Path('/home/pi/gratings')  # './dummy_vis'
 # options = {"duration": 2, "angle": 90, "spac_freq": 0.2, "temp_freq": 1}
@@ -21,20 +22,30 @@ gratings_dir = Path('/home/pi/gratings')  # './dummy_vis'
 # rpg.build_grating(gratings_dir / "test3_grating.dat", options)
 
 with rpg.Screen() as myscreen:
-    grating = myscreen.load_grating(gratings_dir / "test0_grating.dat")
+    # grating = myscreen.load_grating(gratings_dir / "test0_grating.dat")
+    ic("15s context_a grating - check if vertical or horizontal")
+    grating = myscreen.load_grating(gratings_dir / "context_a/a_15.grating")
     myscreen.display_grating(grating)
-    time.sleep(3)
+    time.sleep(17)
 
+    ic("2s grating - check if horizontal")
     grating = myscreen.load_grating(gratings_dir / "test1_grating.dat")
     myscreen.display_grating(grating)
-    time.sleep(3)
+    time.sleep(5)
 
+    ic(".5s grating - check if vertical")
     grating = myscreen.load_grating(gratings_dir / "test2_grating.dat")
     myscreen.display_grating(grating)
-    time.sleep(.5)
+    time.sleep(1)
 
+    ic(".5s grating - check if horizontal")
     grating = myscreen.load_grating(gratings_dir / "test3_grating.dat")
     myscreen.display_grating(grating)
+    time.sleep(1)
+
+    myscreen.display_greyscale(40)
+    time.sleep(2)
+    myscreen.display_greyscale(0)
 
 
  # grating = myscreen.load_grating("~/second_grating.dat")
