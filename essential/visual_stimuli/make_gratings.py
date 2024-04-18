2f9f1a962f8133af9e55e2ef5200e9ddd77080db
Matthew Chin <35109891+mattchin35@users.noreply.github.com>
2024-04-17 20:15:19 -0400

    updates for testing visuals

--- essential/visual_stimuli/make_gratings.py
+++ essential/visual_stimuli/make_gratings.py
@@ -1,5 +1,7 @@
 import rpg
+from pathlib import Path
 
+gratings_dir = Path('/home/pi/gratings')  # './dummy_vis'
 
 # options = {"duration": 2, "angle": 90, "spac_freq": 0.2, "temp_freq": 1}
 # rpg.build_grating("~/test_grating.dat", options)
@@ -8,10 +10,10 @@ import rpg
 # rpg.build_grating("~/test_grating.dat", options)
 
 options = {"duration": .5, "angle": 90, "spac_freq": 0.2, "temp_freq": 1}
-rpg.build_grating("~/vertical_grating_pulse.dat", options)
+rpg.build_grating(gratings_dir / "vertical_grating_pulse.dat", options)
 
 options = {"duration": .5, "angle": 0, "spac_freq": 0.2, "temp_freq": 1}
-rpg.build_grating("~/horizontal_grating_pulse.dat", options)
+rpg.build_grating(gratings_dir / "horizontal_grating_pulse.dat", options)
 
 with rpg.Screen() as myscreen:
     grating = myscreen.load_grating("~/second_grating.dat")
