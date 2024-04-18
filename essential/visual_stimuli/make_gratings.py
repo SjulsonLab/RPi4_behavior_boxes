c20fd774993c2bf85d4bcfa50ff1be204892e487
Matthew Chin <35109891+mattchin35@users.noreply.github.com>
2024-04-17 20:21:15 -0400

    updates for testing visuals

--- essential/visual_stimuli/make_gratings.py
+++ essential/visual_stimuli/make_gratings.py
@@ -9,13 +9,18 @@ gratings_dir = Path('/home/pi/gratings')  # './dummy_vis'
 # options = {"duration": 2, "angle": 0, "spac_freq": 0.2, "temp_freq": 1}
 # rpg.build_grating("~/test_grating.dat", options)
 
-options = {"duration": .5, "angle": 90, "spac_freq": 0.2, "temp_freq": 1}
-rpg.build_grating(gratings_dir / "vertical_grating_pulse.dat", options)
+# options = {"duration": .5, "angle": 90, "spac_freq": 0.2, "temp_freq": 1}
+# rpg.build_grating(gratings_dir / "vertical_grating_.5s.dat", options)
+#
+# options = {"duration": .5, "angle": 0, "spac_freq": 0.2, "temp_freq": 1}
+# rpg.build_grating(gratings_dir / "horizontal_grating_.5s.dat", options)
 
-options = {"duration": .5, "angle": 0, "spac_freq": 0.2, "temp_freq": 1}
-rpg.build_grating(gratings_dir / "horizontal_grating_pulse.dat", options)
+durations = [.5, 1, 2]
 
-with rpg.Screen() as myscreen:
-    grating = myscreen.load_grating("~/second_grating.dat")
-    myscreen.display_grating(grating)
+for duration in durations:
+    options = {"duration": duration, "angle": 90, "spac_freq": 0.2, "temp_freq": 1}
+    rpg.build_grating(gratings_dir / f"vertical_grating_{duration}s.dat", options)
+
+    options = {"duration": duration, "angle": 0, "spac_freq": 0.2, "temp_freq": 1}
+    rpg.build_grating(gratings_dir / f"horizontal_grating_{duration}s.dat", options)
 
