c55d6eae9982f3823b20299f9d085bc9fda3c28c
jjouvion <165422261+jjouvion@users.noreply.github.com>
2024-04-29 17:12:02 -0400

    Update vstest_diagonal.py

--- vstest_diagonal.py
+++ vstest_diagonal.py
@@ -2,10 +2,10 @@
 ################
 import rpg
 options_go = {"duration": 3, "angle": 45, "spac_freq": 0.3, "temp_freq": 2}
-rpg.build_grating("~/first_grating_go.dat", options_go)
+rpg.build_grating("~/first_grating_go_diag.dat", options_go)
 
 options_nogo = {"duration": 3, "angle": 135, "spac_freq": 0.3, "temp_freq": 2}
-rpg.build_grating("~/first_grating_nogo.dat", options_nogo)
+rpg.build_grating("~/first_grating_nogo_diag.dat", options_nogo)
 
 with rpg.Screen() as myscreen:
  grating_go = myscreen.load_grating("~/first_grating_go.dat")
