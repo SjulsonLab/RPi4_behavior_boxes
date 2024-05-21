e1ab65400eff9883b5f3f081e3e519c73831a6ed
mitchfarrell <mrfarrel@uci.edu>
2024-05-21 17:04:14 -0400

    OFT updates

--- opioid_forage_task_phase_1.py
+++ opioid_forage_task_phase_1.py
@@ -231,7 +231,7 @@ class OpioidForageTaskPhase1(object):
         current_time = time.time()
         self.event_times.append(current_time)
 
-        fig, ax = plt.subplots(3, 1, figsize=(5, 4))
+        fig, ax = plt.subplots(3, 1, figsize=(8, 6))
         ax[0].plot(self.timeline_right_entry, np.arange(1, len(self.timeline_right_entry) + 1), 'r-', label='Right Entry')
         ax[0].set_title('Right Entry Events Over Time')
         ax[0].set_xlabel('Time (s)')
