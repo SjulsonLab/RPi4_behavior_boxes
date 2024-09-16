fe3925f7a05575153f180bd255c6324b84fd375e
gneubertda <gabriela.neubertdasilva@einsteinmed.edu>
2024-09-16 09:31:03 -0400

    Create vstest_shapes.py

---
+++ vstest_shapes.py
@@ -0,0 +1,35 @@
+#Test file from https://github.com/bill-connelly/rpg 
+################
+import rpg
+
+# Define the "Go" stimulus as a black circle
+options_go = {
+    "shape": "circle",               # Defining it as a circle
+    "color": (0, 0, 0),              # Black color
+    "duration": 3,                   # Duration in seconds
+    "percent_diameter": 50,          # Diameter of the circle as a percentage of the screen
+    "percent_center_left": 50,       # Center of the circle in x
+    "percent_center_top": 50         # Center of the circle in y
+}
+rpg.build_grating("~/first_grating_go_shape.dat", options_go)
+
+# Define the "No-Go" stimulus as a black square
+options_nogo = {
+    "shape": "square",               # Defining it as a square
+    "color": (0, 0, 0),              # Black color
+    "duration": 3,                   # Duration in seconds
+    "percent_size": 30,              # Size of the square as a percentage of the screen
+    "percent_center_left": 50,       # Center of the square in x
+    "percent_center_top": 50         # Center of the square in y
+}
+rpg.build_grating("~/first_grating_nogo_shape.dat", options_nogo)
+
+
+# Running both "Go" (circle) and "No-Go" (square) stimuli
+
+with rpg.Screen() as myscreen:
+    grating_go = myscreen.load_grating("~/first_grating_go_shape.dat")
+    myscreen.display_grating(grating_go)
+  
+    grating_nogo = myscreen.load_grating("~/first_grating_nogo_shape.dat")
+    myscreen.display_grating(grating_nogo)
