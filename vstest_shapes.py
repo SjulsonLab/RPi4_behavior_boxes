84653b925bde2fe9d0a24b918f82500ea36530f7
gneubertda <gabriela.neubertdasilva@einsteinmed.edu>
2024-09-17 09:34:57 -0400

    Update vstest_shapes.py

--- vstest_shapes.py
+++ vstest_shapes.py
@@ -2,16 +2,14 @@
 ################
 import rpg
 
-# Define the "Go" stimulus as a black circle
-options_go = {"shape": "circle", "color": (0, 0, 0), "angle": 0, "spac_freq": 0.1, "temp_freq": 0.3, "duration": 3, "percent_diameter": 50, "percent_center_left": 50, "percent_center_top": 50}
-rpg.build_grating("~/first_grating_go_shape.dat", options_go)
+# Define the "Go" stimulus
+options_go = {"angle": 90, "spac_freq": 0.1, "temp_freq": 0.3, "duration": 3, "percent_diameter": 50, "percent_center_left": 50, "percent_center_top": 50}
+rpg.build_masked_grating("~/first_grating_go_shape.dat", options_go)
 
-# Define the "No-Go" stimulus as a black square
-options_nogo = {"shape": "square", "color": (0, 0, 0), "angle": 0, "spac_freq": 0.1, "temp_freq": 0.3, "duration": 3, "percent_size": 30, "percent_center_left": 50, "percent_center_top": 50}
-rpg.build_grating("~/first_grating_nogo_shape.dat", options_nogo)
+# Define the "No-Go" stimulus
+options_nogo = {"angle": 180, "spac_freq": 0.1, "temp_freq": 0.3, "duration": 3, "percent_size": 30, "percent_center_left": 50, "percent_center_top": 50}
+rpg.build_masked_grating("~/first_grating_nogo_shape.dat", options_nogo)
 
-
-# Running both "Go" (circle) and "No-Go" (square) stimuli
 
 with rpg.Screen() as myscreen:
     grating_go = myscreen.load_grating("~/first_grating_go_shape.dat")
