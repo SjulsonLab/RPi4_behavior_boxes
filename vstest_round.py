be68ef120464aefe3101ecd2b26d79d7cbf4c576
gneubertda <gabriela.neubertdasilva@einsteinmed.edu>
2024-09-17 15:23:36 -0400

    Update vstest_round.py

--- vstest_round.py
+++ vstest_round.py
@@ -3,11 +3,11 @@
 import rpg
 
 # Define the "Go" stimulus
-options_go = {"angle": 90, "spac_freq": 0.1, "temp_freq": 0.3, "duration": 3,  "contrast": 0, "percent_diameter": 30, "percent_center_left": 50, "percent_center_top": 50, "background": 127}
+options_go = {"angle": 90, "spac_freq": 0.1, "temp_freq": 0.3, "duration": 3,  "contrast": 0, "percent_diameter": 30, "percent_center_left": 50, "percent_center_top": 50, "background": 0}
 rpg.build_masked_grating("~/first_grating_go_round.dat", options_go)
 
 # Define the "No-Go" stimulus
-options_nogo = {"angle": 180, "spac_freq": 0.1, "temp_freq": 0.3, "duration": 3, "contrast": 0, "percent_diameter": 30, "percent_center_left": 50, "percent_center_top": 50,  "background": 255}
+options_nogo = {"angle": 180, "spac_freq": 0.1, "temp_freq": 0.3, "duration": 3, "contrast": 0, "percent_diameter": 70, "percent_center_left": 50, "percent_center_top": 50,  "background": 127}
 rpg.build_masked_grating("~/first_grating_nogo_round.dat", options_nogo)
 
 
