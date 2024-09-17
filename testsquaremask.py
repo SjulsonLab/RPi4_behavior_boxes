fbd0e052268980f30c3012dc3d094dc5110bd09e
gneubertda <gabriela.neubertdasilva@einsteinmed.edu>
2024-09-17 18:07:29 -0400

    Update testsquaremask.py

--- testsquaremask.py
+++ testsquaremask.py
@@ -1,6 +1,12 @@
-#Piece of code removed from bill-connelly and modified to build a square maksk
+#Piece of code removed from bill-connelly and modified to build a square maksk - IN PROCESS
 
-import rpg
+import RPG
+
+# Define the "Go" stimulus - white circle, black background
+options_go = {"angle": 90, "spac_freq": 0.1, "temp_freq": 0.3, "duration": 3,  "contrast": 0, "percent_diameter": 50, "percent_center_left": 50, "percent_center_top": 50, "background": 0}
+rpg.build_masked_grating("~/first_grating_go_round.dat", options_go)
+
+# Define the "No Go" stimulus - black square, white background
 
 void* square(int waveform, int square_size, int padding, int point_x, int point_y, int center_x, int center_y, int t, int wavelength, int speed, 
              double angle, double cosine, double sine, double weight, double contrast, int background, int colormode){
@@ -58,3 +64,41 @@ void* square(int waveform, int square_size, int padding, int point_x, int point_
 
     return pixel_ptr;
 }
+#CONTINUE HERE 
+
+int build_grating(char * filename, double duration, double angle, double sf, double tf, double contrast, int background, int width, int height, int waveform, double 
+	percent_sigma, double percent_diameter, double percent_center_left, double percent_center_top, double percent_padding, int colormode){ 
+
+  #int left_boundary = center_x - (square_size / 2) - padding;
+    int right_boundary = center_x + (square_size / 2) + padding;
+    int top_boundary = center_y - (square_size / 2) - padding;
+    int bottom_boundary = center_y + (square_size / 2) + padding;
+
+	int fps = get_refresh_rate();
+        printf("Refresh rate measured as: %d hz\n", fps);
+	fb_config fb0;
+	fb0.width = width;
+	fb0.height = height;
+	fb0.depth = (colormode==RGB888MODE)?24:16;
+	fb0.size = (fb0.height)*(fb0.depth)*(fb0.width)/8; //8 bits/byte
+	FILE * file = fopen(filename, "wb");
+	if(file == NULL){
+		perror("File creation failed\n");
+		PyErr_SetString(PyExc_OSError,"File creation failed.");
+		return 1;
+	}
+	int wavelength = (fb0.width/DEGREES_SUBTENDED)/sf;
+
+	int speed = wavelength*tf/fps;
+	if(speed==0){
+		speed = 1;
+	}
+	double actual_tf = ((double)(speed*fps)) / wavelength;
+	int sigma = fb0.width * percent_sigma / 100;
+	int radius = fb0.width * percent_diameter / 200;
+	int center_j = fb0.width * percent_center_left / 100;
+	int center_i = fb0.height * percent_center_top / 100;
+	double padding = radius * percent_padding / 100;
+	if(actual_tf!=tf){
+		printf("Grating %s has a requested temporal frequency of %f, actual temporal frequency will be %f\n",filename,tf,actual_tf);
+	}
