5e970c0d12fc73f92e58c35aacca11171bdbfebb
gneubertda <gabriela.neubertdasilva@einsteinmed.edu>
2024-09-17 10:59:44 -0400

    Update and rename vstest_shapes.py to vstest_roundmask.py

--- vstest_roundmask.py
+++ vstest_roundmask.py
@@ -3,12 +3,12 @@
 import rpg
 
 # Define the "Go" stimulus
-options_go = {"angle": 90, "spac_freq": 0.1, "temp_freq": 0.3, "duration": 3, "percent_diameter": 30, "percent_center_left": 50, "percent_center_top": 50, "background": 0, "waveform": rpg.SQUARE}
-rpg.build_masked_grating("~/first_grating_go_shape.dat", options_go)
+options_go = {"angle": 90, "spac_freq": 0.1, "temp_freq": 0.3, "duration": 3, "percent_size": 30, "percent_center_left": 50, "percent_center_top": 50, "background": 0, "waveform": rpg.SQUARE}
+rpg.build_masked_grating("~/first_grating_go_round.dat", options_go)
 
 # Define the "No-Go" stimulus
 options_nogo = {"angle": 180, "spac_freq": 0.1, "temp_freq": 0.3, "duration": 3, "percent_size": 30, "percent_center_left": 50, "percent_center_top": 50,  "background": 255, "waveform": rpg.SQUARE}
-rpg.build_masked_grating("~/first_grating_nogo_shape.dat", options_nogo)
+rpg.build_masked_grating("~/first_grating_nogo_round.dat", options_nogo)
 
 
 with rpg.Screen() as myscreen:
