09104e217b929faf2ef34c69b421600a4f6b1167
gneubertda <gabriela.neubertdasilva@einsteinmed.edu>
2024-09-18 09:17:26 -0400

    Update testsquaremask.py

--- testsquaremask.py
+++ testsquaremask.py
@@ -5,7 +5,7 @@ import RPG
 # Define the "Go" stimulus - white circle, black background
 
 options_go = {"angle": 90, "spac_freq": 0.1, "temp_freq": 0.3, "duration": 3,  "contrast": 0, "percent_diameter": 50, "percent_center_left": 50, "percent_center_top": 50, "background": 0}
-rpg.build_masked_grating("~/first_grating_go_round.dat", options_go)
+rpg.build_masked_grating("~/first_grating_go_smask.dat", options_go)
 
 # Define the "No Go" stimulus - black square, white background
 
@@ -207,4 +207,12 @@ int build_grating(char * filename, double duration, double angle, double sf, dou
 }
 
 options_nogo = {"angle": 90, "spac_freq": 0.1, "temp_freq": 0.3, "duration": 3,  "contrast": 0, "colormode": 0, "percent_side_length": 50, "percent_center_left": 50, "percent_center_top": 50, "background": 255}
-rpg.build_square_shape_grating("~/first_grating_nogo_round.dat", options_nogo)
+rpg.build_square_shape_grating("~/first_grating_nogo_smask.dat", options_nogo)
+
+
+with rpg.Screen() as myscreen:
+    grating_go = myscreen.load_grating("~/first_grating_go_smask.dat")
+    myscreen.display_grating(grating_go)
+  
+    grating_nogo = myscreen.load_grating("~/first_grating_nogo_smask.dat")
+    myscreen.display_grating(grating_nogo)
