369225bd68e23a776ef0701b23693744c64d547c
juliabenville <julia.benville@einsteinmed.edu>
2024-09-26 13:55:55 -0400

    Update julia_DCL_self_admin.py
    updates from the session info file

--- julia_DCL_self_admin.py
+++ julia_DCL_self_admin.py
@@ -4,14 +4,13 @@
 """
 author: Julia Benville
 date: 2024-08-05
-name: JB_Cocaine_Cue_Learning.py (adapted from remi_self_admin_lever_task.py)
+name: julia_DCL_self_admin.py (adapted from remi_self_admin_lever_task.py)
 """
 
 import importlib
 from transitions import Machine, State
 from transitions.extensions.states import add_state_features, Timeout
 import pysistence
-from icecream import ic
 import logging
 import time
 from datetime import datetime
@@ -39,7 +38,6 @@ logging.config.dictConfig(
 class TimedStateMachine(Machine):
     pass
 
-
 class CocaineSelfAdminLeverTask(object):
     def __init__(self, **kwargs):
         # Set name and session_info
@@ -56,13 +54,19 @@ class CocaineSelfAdminLeverTask(object):
         else:
             self.session_info = kwargs.get("session_info")
 
-        ic(self.session_info)
+        # Access mouse weight from session info
+        self.mouse_weight = self.session_info['weight']  # Read mouse weight from session_info
+
+        # Define drug dosage parameters
+        self.dosage_mg_per_kg = 0.75  # 0.75 mg/kg
+        self.infusion_rate_ul_per_sec = 3.125  # Infusion rate in µL/sec
+        self.bolus_volume_ul = 12.5  # Bolus volume per infusion in µL
 
         # Define states and transitions
         self.states = [
             State(name='standby', on_exit=["exit_standby"]),
             State(name="reward_available", on_enter=["enter_reward_available"], on_exit=["exit_reward_available"]),
-            Timeout(name='timeout', on_enter=['enter_timeout'], on_exit=['exit_timeout'], timeout=20, on_timeout=['switch_to_reward_available']),  # Timeout lasts 20 seconds after infusion
+            Timeout(name='timeout', on_enter=['enter_timeout'], on_exit=['exit_timeout'], timeout=20, on_timeout=['switch_to_reward_available']),
             Timeout(name='cath_fill', on_enter=['enter_cath_fill'], on_exit=['exit_cath_fill'], timeout=self.session_info['cath_fill'], on_timeout=['switch_to_reward_available'])
         ]
 
@@ -78,103 +82,61 @@ class CocaineSelfAdminLeverTask(object):
 
         self.machine = TimedStateMachine(model=self, states=self.states, transitions=self.transitions, initial='standby')
 
-        # Initialize variables for trials
-        self.trial_running = False
-        self.trial_number = 0
-        self.entry_interval = self.session_info['entry_interval']
-        self.reward_time = 10
-        self.reward_list = []
-        self.infusions = 0  # Track the number of infusions
-        self.start_time = time.time()  # Record session start time
-        
-        # Initialize behavior box
-        self.box = behavbox.BehavBox(self.session_info)
-        self.syringe_pump = LED(17)
-        self.total_reward = 0
+        self.syringe_pump = PWMLED(17)
+        self.infusions = 0
+        self.start_time = time.time()
+
+    def calculate_infusion_duration(self):
+        # Calculate drug infusion duration based on bolus volume and infusion rate
+        dose_volume = self.bolus_volume_ul
+        infusion_duration = dose_volume / self.infusion_rate_ul_per_sec
+        return infusion_duration
 
     def reward(self):
-        # Updated for 0.75 mg/kg dose with a 12.5 µL infusion per bolus
-        infusion_duration = 12.5 / 3.125  # 12.5 µL at 3.125 µL/sec rate
-        self.syringe_pump.blink(infusion_duration, 0.1, 1)  # Infuse drug for calculated duration
-        self.reward_list.append(("syringe_pump_reward", infusion_duration))
-        logging.info(";" + str(time.time()) + ";[reward];syringe_pump_reward;" + str(infusion_duration))
+        # Infusion duration calculation
+        infusion_duration = self.calculate_infusion_duration()
+
+        # Infuse drug for calculated duration
+        self.syringe_pump.blink(infusion_duration, 0.1, 1)
+        logging.info(f";{time.time()};[reward];syringe_pump_reward;{infusion_duration}")
 
     def fill_cath(self):
-        # Update to fill catheter with ~12 µL
-        infusion_duration = 12 / 3.125  # 12 µL at 3.125 µL/sec rate
-        self.syringe_pump.blink(infusion_duration, 0.1, 1)  # Infuse ~12 µL
-        logging.info(";" + str(time.time()) + ";[reward];catheter_filled_with_~12ul;" + str(infusion_duration) + "_second_infusion")
+        # Use 'cath_fill' value from session_info for the catheter fill duration
+        self.syringe_pump.blink(self.session_info['cath_fill'], 0.1, 1)
+        logging.info(";" + str(time.time()) + ";[reward];catheter_filled_with_~12ul")
 
     def run(self):
         if self.state in ["standby", "timeout"]:
             pass
         elif self.state == 'reward_available':
-            if self.box.event_list:
-                self.event_name = self.box.event_list.popleft()
-            else:
-                self.event_name = ''
-
-            # Check for session end (40 infusions or 2 hours)
             if self.infusions >= 40 or (time.time() - self.start_time) >= 7200:
                 self.end_session()
                 return
-
-            if self.event_name == 'right_entry':
-                self.process_active_lever_press()
-
-        self.box.check_keybd()
-
-    def process_active_lever_press(self):
-        logging.info(";" + str(time.time()) + ";[lever_press];active_lever_pressed;")
-        self.box.cueLED2.off()  # Turn off LED immediately at time 0
-        self.box.sound2.on()  # Play sound immediately at time 0
-        
-        # Schedule LED and sound events after specific delays
-        threading.Timer(2, self._handle_led_sound_switch).start()  # Switch LED on and sound off at second 2
-        threading.Timer(3, self.reward).start()  # Start the infusion at second 3
-        
-        self.infusions += 1
-        # Start the 20-second timeout after the 3-second infusion
-        threading.Timer(3, self.switch_to_timeout).start()  # Switch to timeout 3 seconds after the lever press
-
-    def _handle_led_sound_switch(self):
-        self.box.sound2.off()  # Turn sound off at second 2
-        self.box.cueLED2.on()  # Turn LED back on at second 2
+            self.infusions += 1
+            threading.Timer(3, self.reward).start()  # Start infusion 3 seconds after lever press
+            threading.Timer(3, self.switch_to_timeout).start()  # Switch to timeout 3 seconds after lever press
 
     def enter_standby(self):
         logging.info(";" + str(time.time()) + ";[transition];enter_standby;")
-        self.trial_running = False
-        self.box.event_list.clear()
 
     def exit_standby(self):
         logging.info(";" + str(time.time()) + ";[transition];exit_standby;")
-        self.box.event_list.clear()
         self.fill_cath()
 
     def enter_reward_available(self):
         logging.info(";" + str(time.time()) + ";[transition];enter_reward_available;")
-        self.box.cueLED2.on()
-        self.box.event_list.clear()
-        self.trial_running = True
 
     def exit_reward_available(self):
         logging.info(";" + str(time.time()) + ";[transition];exit_reward_available;")
-        self.box.cueLED2.off()
-        self.box.event_list.clear()
 
     def enter_timeout(self):
         logging.info(";" + str(time.time()) + ";[transition];enter_timeout;")
-        self.trial_running = False
-        self.box.sound2.on()
 
     def exit_timeout(self):
         logging.info(";" + str(time.time()) + ";[transition];exit_timeout;")
-        self.box.sound2.off()
 
     def enter_cath_fill(self):
         logging.info(";" + str(time.time()) + ";[transition];enter_cath_fill;")
-        self.trial_running = False
-        self.box.event_list.clear()
 
     def exit_cath_fill(self):
         logging.info(";" + str(time.time()) + ";[transition];exit_cath_fill;")
@@ -182,7 +144,7 @@ class CocaineSelfAdminLeverTask(object):
 
     def enter_session_end(self):
         logging.info(";" + str(time.time()) + ";[transition];enter_session_end;")
-        self.box.cueLED2.off()
+        self.syringe_pump.off()
 
     def end_session(self):
         self.state = "session_end"
@@ -190,10 +152,3 @@ class CocaineSelfAdminLeverTask(object):
 
     def end_task(self):
         logging.info("Ending task...")
-        self.box.clean_exit()
-
-
-# To run as a script
-if __name__ == "__main__":
-    task = CocaineSelfAdminLeverTask(name="MouseTest", session_info={'weight': 30, 'timeout_time': 23, 'cath_fill': 3})
-    task.run()
