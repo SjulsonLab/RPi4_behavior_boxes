d4bbd0917c52431e71463f6f2f4ae1d283cc21b0
mattchin35 <mattchin35@gmail.com>
2024-12-17 12:21:43 -0500

    transfer_files call debug

--- transfer_files.sh
+++ transfer_files.sh
@@ -2,13 +2,15 @@
 
 # $1 is the video pi IP address, $2 is the buffer directory, $3 is the external HD directory, $4 is a video sync boolean
 if [ "$4" = "True" ]; then
+  echo ""
   echo "Transferring Raspberry Pi video"
   rsync -av --progress --remove-source-files "pi@$1:$2/ $3"
   rsync -av --progress --remove-source-files "pi@$1:~/video/*.log $3"
 else
+  echo ""
   echo "No Raspberry Pi video to save"
 fi
 
 echo "Transferring buffer files"
-echo "rsync -arvz --progress --remove-source-files $2/ $3/"
-rsync -arvz --progress --remove-source-files "$2/ $3/"
+echo "rsync -arvz --progress --remove-source-files $2 $3"
+rsync -arvz --progress --remove-source-files "$2 $3"
