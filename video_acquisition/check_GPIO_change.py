f631c7c960c8c02c64b54eb7ebe218d750d9b8a5
mattchin35 <mattchin35@gmail.com>
2025-04-08 17:47:55 -0400

    debugging PiCamera2 updates

--- video_acquisition/check_GPIO_change.py
+++ video_acquisition/check_GPIO_change.py
@@ -6,54 +6,76 @@ import concurrent.futures
 # Set up GPIO mode
 GPIO.setmode(GPIO.BCM)
 
-# Define the GPIO pin number
-pin_number = 4
-
 # Set up the GPIO pin as an input
-# GPIO.setup(pin_number, GPIO.IN)
-GPIO.setup(pin_number, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(pin_flipper, GPIO.BOTH, bouncetime=100)
-GPIO.add_event_callback(pin_number, flipper_timestamps_detect)
-previous_state = GPIO.input(pin_number)
-print("Start state is {}".format(previous_state))
-
+pin_flipper = 4
+GPIO.setup(pin_flipper, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+previous_state = GPIO.input(pin_flipper)
 state_change = threading.Event()
 
-def check_input_change(previous_state):
-    current_state = GPIO.input(pin_number)
-    if current_state != previous_state:
-        if current_state == GPIO.HIGH:
-            print("GPIO pin is HIGH")
-        else:
-            print("GPIO pin is LOW")
-        previous_state = current_state
-    return current_state
+class FlipperInput:
+    def __init__(self, pin_number):
+        self.pin_number = pin_number
+        self.flip_state = GPIO.input(pin_number)
+        print("Start state is {}".format(previous_state))
+        self._flipper_timestamps = []
+        self.flip_thread = None
+        GPIO.add_event_detect(pin_number, GPIO.BOTH, bouncetime=100)
 
+    def get_flipper_timestamps(self):
+        return self._flipper_timestamps
 
-def flipper_timestamps_detect():
-    input_state = GPIO.input(pin_number)
-    GPIO.remove_event_detect(pin_number)
-    print(input_state, time.time())
-    GPIO.add_event_detect(pin_flipper, GPIO.BOTH, bouncetime=BOUNCETIME)
+    def set_flipper_timestamps(self, timestamps):
+        self._flipper_timestamps = timestamps
 
-
-try:
-    while True:
-        # Read the current state of the GPIO pin
-        current_state = GPIO.input(pin_number)
-
-        # Check if the state has changed
-        if current_state != previous_state:
-            if current_state == GPIO.HIGH:
-                print("GPIO pin is HIGH")
+    def GPIO_loop(self, bouncetime=100):
+        while True:
+            cur_state = GPIO.input(pin_flipper)
+            if cur_state != self.flip_state:
+                if cur_state == GPIO.HIGH:
+                    print("GPIO pin is HIGH")
+                else:
+                    print("GPIO pin is LOW")
+                state_change.set()
+                self.flip_state = cur_state
+                time.sleep(bouncetime / 1000)  # Convert milliseconds to seconds
             else:
-                print("GPIO pin is LOW")
+                state_change.clear()
+                time.sleep(.001)  # Sleep for a short time to avoid busy waiting
 
-            # Update the previous state
-            previous_state = current_state
+    def event_loop(self):
+        while True:
+            if state_change.is_set():
+                self.flipper_callback()
+                state_change.clear()
+            else:
+                time.sleep(0.001)
 
-        # Wait for 20 milliseconds before checking again
-        time.sleep(0.02)
+    def flipper_callback(self):
+        self._flipper_timestamps.append((self.flip_state, time.time()))
+        print(self.flip_state, time.time())
+        # print(str(self._flipper_timestamps))
+
+    def start_flipper_thread(self):
+        if self.flip_thread is None:
+            self.flip_thread = threading.Thread(target=self.GPIO_loop)
+            self.flip_thread.start()
+        else:
+            print("Flipper thread already running")
+
+
+flipper = FlipperInput(pin_flipper)
+try:
+    # Start the GPIO loop in a separate thread
+    flipper.start_flipper_thread()
+    tstart = time.perf_counter()
+    while True:
+        # do pseudo work
+        time.sleep(1/60)  # 60 FPS
+
+        if time.perf_counter() - tstart >= 1:
+            print("Flipper timestamps: {}".format(flipper.get_flipper_timestamps()))
+            # Wait for 20 milliseconds before checking again
+            time.sleep(0.02)
 
 except KeyboardInterrupt:
     # Clean up GPIO settings before exiting
