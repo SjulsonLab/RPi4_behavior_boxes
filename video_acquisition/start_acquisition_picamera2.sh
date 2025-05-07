47d63ef7e584f23b721442e4cdd871a6e260e905
mattchin35 <mattchin35@gmail.com>
2025-05-07 15:03:04 -0400

    adding start_acquisition_picamera2.sh

---
+++ video_acquisition/start_acquisition_picamera2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# $1 is the video output directory, $2 is the file basename
+
+if [ ! -d "$1" ]; then
+    echo "Creating directory $1"
+    mkdir -p "$1"
+fi
+
+echo "Starting video log"
+date >> "$1/videolog.log"
+
+echo "Starting video acquisition"
+nohup python3 /home/pi/RPi4_behavior_boxes/video_acquisition/start_acquisition_picamera2.py "$2" >> "$1/videolog.log" 2>&1 &
+
+exit 0
