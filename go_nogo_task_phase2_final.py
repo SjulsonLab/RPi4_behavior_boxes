79322282252789baf7b94dffaf196594a44e7009
Duy Tran <90647811+dtran1125@users.noreply.github.com>
2025-05-29 11:42:53 -0400

    Update go_nogo_task_phase2_final.py
    update

--- go_nogo_task_phase2_final.py
+++ go_nogo_task_phase2_final.py
@@ -385,9 +385,12 @@ class go_nogo_phase2(object):
         # This function asks the user to input whether they want reward delivery
         # This is used to bait the animal to lick initially
         # If y, deliver reward, if hit enter, start random reward phase
-        self.deliver_reward = input("Deliver reward, else start allgo? (y or hit enter): \n")
-        if self.deliver_reward == "y":
+        self.deliver_reward = input("Deliver reward, else start allgo? (hit enter OR hit y then enter): \n")
+        count_time = 1
+        if self.deliver_reward == "":
             self.pump.reward("1", self.session_info["solenoid_blink_duration"], 0.01, 6)
+            logging.info(str(time.time()) + ", reward count " + str(count_time))
+            count_time = count_time + 1
 
     ########################################################################
     # countdown methods to run when vstim starts to play, used as timers since vstim starts
