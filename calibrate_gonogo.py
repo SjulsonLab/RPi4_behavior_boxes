d7f249d19fd8508907b33af678912d87a60ef286
juliabenville <julia.benville@einsteinmed.edu>
2025-06-03 15:17:56 -0400

    Update calibrate_gonogo.py

--- calibrate_gonogo.py
+++ calibrate_gonogo.py
@@ -69,22 +69,22 @@ while True:
     # pump.reward(pump_number, on_duration, off_duration, pulse_time)
     if pump_number == "1":
         LED(19).blink(on_time, off_time, iteration)
-        print("pump1, " + str(on_time) + str(off_time) + str(iteration))
+        print("pump1, " + str(on_time) + str(off_time) + str(pulse_time))
     elif pump_number == "2":
         LED(20).blink(on_time, off_time, iteration)
-        print("pump2, " + str(on_time) + str(off_time) + str(iteration))
+        print("pump2, " + str(on_time) + str(off_time) + str(pulse_time))
     elif pump_number == "3":
         LED(21).blink(on_time, off_time, iteration)
-        print("pump3, " + str(on_time) + str(off_time) + str(iteration))
+        print("pump3, " + str(on_time) + str(off_time) + str(pulse_time))
     elif pump_number == "4":
-        LED(7).blink(on_time, off_time, iteration)
-        print("pump4, " + str(on_time) + str(off_time) + str(iteration))
+        LED(7).blink(on_time, off_time, pulse_time)
+        print("pump4, " + str(on_time) + str(off_time) + str(pulse_time))
     time.sleep((on_time+off_time)*pulse_time + 0.1)
     print("Please go weight the container with the liquid!\n")
     weight_total = float(input("weight_total: "))
     weight_fluid = weight_total - weight_tube
     calibration_log.append(
-        (pump_number, on_duration, off_duration,
+        (pump_number, on_time, off_time,
          pulse_time, weight_tube, weight_total, weight_fluid)
     )
     abort_or_not = str(input("Abort the program?(Y/N) \n")).upper()
