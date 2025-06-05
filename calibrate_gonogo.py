f44e01e1c80f0a0eb443b8b8a3294146a6150a3e
Duy Tran <90647811+dtran1125@users.noreply.github.com>
2025-06-05 11:47:32 -0400

    Update calibrate_gonogo.py

--- calibrate_gonogo.py
+++ calibrate_gonogo.py
@@ -8,56 +8,11 @@ description:
 
 """
 from datetime import datetime
-import io
-from subprocess import check_output
 from gpiozero import LED
 import time
 
 datestr = str(datetime.now().strftime("%Y-%m-%d"))
 timestr = str(datetime.now().strftime('%H%M%S'))
-calibrator = str(input("Who is calibrating the pump? (Enter your name)"))
-box_number = check_output(['hostname', '-I']).decode('ascii')[-5:-2]
-base_path = "/home/pi/experiment_info/calibration_info/"
-calibration_filename = base_path + "calibration_box" + str(box_number) + "_" + str(
-    calibrator) + "_" + datestr + timestr + '.csv'
-# initiate the calibration list for logging at the end
-calibration_log = []  # initiate calibration list for appending
-
-
-def calibration_flush(calibration_filename, calibration_log):
-    print("Flushing: " + calibration_filename)
-    with io.open(calibration_filename, 'w') as f:
-        f.write('pump_number, on_time, off_time, pulse_time, weight_tube, weight_total, weight_fluid\n')
-        for entry in calibration_log:
-            f.write('%f, %f, %f, %f, %f, %f, %f\n' % entry)
-
-
-# class Pump(object):  # specifically for calibration, different from the behavbox pump object
-#     def __init__(self):
-#         self.pump1 = LED(19)
-#         self.pump2 = LED(20)
-#         self.pump3 = LED(21)
-#         self.pump4 = LED(7)
-#         self.pump_air = LED(8)
-#         self.pump_vacuum = LED(25)
-#
-#     def reward(self, which_pump, on_time, off_time, iteration):
-#         if which_pump == "1":
-#             self.pump1.blink(on_time, off_time, iteration)
-#             print("pump1, " + str(on_time) + str(off_time) + str(iteration))
-#         elif which_pump == "2":
-#             self.pump2.blink(on_time, off_time, iteration)
-#             print("pump2, " + str(on_time) + str(off_time) + str(iteration))
-#         elif which_pump == "3":
-#             self.pump3.blink(on_time, off_time, iteration)
-#             print("pump3, " + str(on_time) + str(off_time) + str(iteration))
-#         elif which_pump == "4":
-#             self.pump4.blink(on_time, off_time, iteration)
-#             print("pump4, " + str(on_time) + str(off_time) + str(iteration))
-
-
-# initiate pump
-# pump = Pump()
 
 while True:
     pump_number = str(input("Pump Number: "))  # user inputs the pump number they intend to calibrate at the moment
@@ -65,8 +20,7 @@ while True:
     off_time = float(input("off_time: "))
     pulse_time = int(input("iteration: "))
     weight_tube = float(input("weight_tube: "))
-    # deliver the water using the pump object
-    # pump.reward(pump_number, on_duration, off_duration, pulse_time)
+
     if pump_number == "1":
         LED(19).blink(on_time, off_time, pulse_time)
         print("pump1, " + str(on_time) + str(off_time) + str(pulse_time))
