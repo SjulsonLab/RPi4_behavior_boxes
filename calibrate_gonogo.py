f399fc41754dc4bfa546bdb37d1c0706456bfbab
Duy Tran <90647811+dtran1125@users.noreply.github.com>
2025-06-05 12:00:57 -0400

    Update calibrate_gonogo.py

--- calibrate_gonogo.py
+++ calibrate_gonogo.py
@@ -13,33 +13,26 @@ import time
 
 datestr = str(datetime.now().strftime("%Y-%m-%d"))
 timestr = str(datetime.now().strftime('%H%M%S'))
+pump_number = str(input("Pump Number: "))  # user inputs the pump number they intend to calibrate at the moment
+on_time = float(input("on_time: "))
+off_time = float(input("off_time: "))
+iteration = int(input("iteration: "))
+weight_tube = float(input("weight_tube: "))
 
-while True:
-    pump_number = str(input("Pump Number: "))  # user inputs the pump number they intend to calibrate at the moment
-    on_time = float(input("on_time: "))
-    off_time = float(input("off_time: "))
-    pulse_time = int(input("iteration: "))
-    weight_tube = float(input("weight_tube: "))
-
+for i in range(iteration):
+    print("reward delivery " + str(i))
     if pump_number == "1":
-        LED(19).blink(on_time, off_time, pulse_time)
-        print("pump1, " + str(on_time) + str(off_time) + str(pulse_time))
+        LED(19).blink(on_time, 0.1, 1)
+        time.sleep(off_time)
     elif pump_number == "2":
-        LED(20).blink(on_time, off_time, pulse_time)
-        print("pump2, " + str(on_time) + str(off_time) + str(pulse_time))
+        time.sleep(off_time)
     elif pump_number == "3":
-        LED(21).blink(on_time, off_time, pulse_time)
-        print("pump3, " + str(on_time) + str(off_time) + str(pulse_time))
+        time.sleep(off_time)
     elif pump_number == "4":
-        LED(7).blink(on_time, off_time, pulse_time)
-        print("pump4, " + str(on_time) + str(off_time) + str(pulse_time))
-    time.sleep((on_time+off_time)*pulse_time + 0.1)
-    print("Please go weight the container with the liquid!\n")
-    weight_total = float(input("weight_total: "))
-    weight_fluid = weight_total - weight_tube
-    print("Fluid weight = " + str(weight_fluid))
-    abort_or_not = str(input("Abort the program?(Y/N) \n")).upper()
-    if abort_or_not == 'Y':
-        break
+        time.sleep(off_time)
 
-print("DONE")
+print("DONE!")
+print("Please go weight the container with the liquid!\n")
+weight_total = float(input("weight_total: "))
+weight_fluid = weight_total - weight_tube
+print("Fluid weight = " + str(weight_fluid))
