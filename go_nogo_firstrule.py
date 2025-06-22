305a6b5a7aca60935129414bedb8aaf28d3cca0f
Duy Tran <90647811+dtran1125@users.noreply.github.com>
2025-06-22 15:54:22 -0400

    Update go_nogo_firstrule.py

--- go_nogo_firstrule.py
+++ go_nogo_firstrule.py
@@ -384,9 +384,9 @@ class go_nogo_firstrule(object):
         # This function asks the user to input whether they want reward delivery
         # This is used to bait the animal to lick initially
         # If y, deliver reward, if hit enter, start random reward phase
-        self.deliver_reward = input("Deliver reward, else start allgo? (hit enter OR hit y then enter): \n")
+        self.deliver_reward = input("Hit enter to deliver reward, or hit y then enter to start allgo: \n")
         if self.deliver_reward == "":
-            self.pump.reward("1", self.session_info["solenoid_blink_duration"], 0.01, 6)
+            self.pump.reward("1", self.session_info["solenoid_blink_duration"], 0.01, 1, background=True)
             
     ########################################################################
     # countdown methods to run when vstim starts to play, used as timers since vstim starts
@@ -447,7 +447,7 @@ class go_nogo_firstrule(object):
             # otherwise transition to vacuum after 1s
             if event_name == "left_entry":
                 self.trial_outcome = 1  # Hit!
-                self.pump.reward("1", self.session_info["solenoid_blink_duration"], 0.01, 6)
+                self.pump.reward("1", self.session_info["solenoid_blink_duration"], 0.01, 1, background=True)
                 logging.info(str(time.time()) + ", reward delivered!")
                 self.time_at_reward = time.time() - self.trial_start_time
                 self.start_temp1()  # trigger state transition to temp1
@@ -534,7 +534,7 @@ class go_nogo_firstrule(object):
 
     def calibrate(self):
         for iteration in range(100):
-            self.pump.reward("1", self.session_info["solenoid_blink_duration"], 0.01, 6)
+            self.pump.reward("1", self.session_info["solenoid_blink_duration"], 0.01, 1, background=True)
             time.sleep(0.5)
         
     ########################################################################
