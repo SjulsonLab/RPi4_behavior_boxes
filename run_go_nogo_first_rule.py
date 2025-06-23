40fb74727d4978264c5677a1c50e20443e114e6b
Duy Tran <90647811+dtran1125@users.noreply.github.com>
2025-06-23 08:19:34 -0400

    Update run_go_nogo_first_rule.py

--- run_go_nogo_first_rule.py
+++ run_go_nogo_first_rule.py
@@ -460,9 +460,8 @@ if __name__ == "__main__":
                                                                          lick_times, vstimON_time, plot_dprime,
                                                                          dprimebinp,))
                 plot_process.start()  # no join because we do not want to wait until the plotting is finished
-                logging.info(str(time.time()) + ", Amount water received " + str(hit_count(i) * session_info['calibrated_drop'])
                 
-            #raise SystemExit
+            raise SystemExit
 
     # graceful exit
     except (KeyboardInterrupt, SystemExit):
