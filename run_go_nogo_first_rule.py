d45c3c65f07371c86d6b59646c86ee0a2f00779d
Duy Tran <90647811+dtran1125@users.noreply.github.com>
2025-06-26 09:19:19 -0400

    Update run_go_nogo_first_rule.py

--- run_go_nogo_first_rule.py
+++ run_go_nogo_first_rule.py
@@ -216,14 +216,14 @@ def plot_trial_progress(current_trial, trial_list, combine_trial_outcome, hit_co
         if found:
             ax4.set_title('CRITERION REACHED!!!', fontsize=13)
             ax4.scatter(indices, dprimebinp[indices], marker='o', color='orange')
-            textstr_dprime = '\n'.join((
-                f"Found {indices[1] - indices[0] + 1} consecutive trials with d' > 2.5",
-                f"Starting at index {indices[0]}, ending at index {indices[1]}"))
-            ax4.text(0.05, 0.95, textstr_dprime, fontsize=11, verticalalignment='top')
+            textstr_dprime = f"Found {indices[1] - indices[0] + 1} consecutive trials with d' > 2.5\nStarting at index {indices[0]}, ending at index {indices[1]}"
+            ax4.text(-0.15, 0.95, textstr_dprime, fontsize=11, verticalalignment='top')
         else:
             ax4.set_title('D-prime', fontsize=11)
-            textstr_dprime = '\n'.join((
-                f"No sequence of 30+ consecutive trials with d' > 2.5 found"))
+            # textstr_dprime = '\n'.join((
+            #     f"No sequence of 30+ consecutive trials with d' > 2.5 found"))
+            textstr_dprime = f"Not learned"
+            
             ax4.text(0.05, 0.95, textstr_dprime, fontsize=11, verticalalignment='top')
 
     ########################################################################
