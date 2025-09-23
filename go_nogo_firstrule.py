7a0e119fb4379bd9a053bf372fb34352124c53b3
juliabenville <julia.benville@einsteinmed.edu>
2025-09-23 12:40:45 -0400

    Update go_nogo_firstrule.py
    commented video start/stop

--- go_nogo_firstrule.py
+++ go_nogo_firstrule.py
@@ -542,9 +542,9 @@ class go_nogo_firstrule(object):
     ########################################################################
     def start_session(self):
         ic("TODO: start video")
-        self.box.video_start()
+        #self.box.video_start()
 
     def end_session(self):
         ic("TODO: stop video")
-        self.box.video_stop()
+        #self.box.video_stop()
         self.box.visualstim_go.myscreen.close()
