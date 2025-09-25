dc189800bf1df7f6013e20cc85126aba2ebdfa94
Duy Tran <90647811+dtran1125@users.noreply.github.com>
2025-09-25 11:29:09 -0400

    Update run_go_nogo_first_rule.py

--- run_go_nogo_first_rule.py
+++ run_go_nogo_first_rule.py
@@ -237,11 +237,11 @@ def plot_trial_progress(current_trial, trial_list, combine_trial_outcome, hit_co
         ax5.set_title('ANIMAL DISENGAGED !!!', fontsize=13)
         ax5.scatter(np.arange(indices_lick_count[0],indices_lick_count[1]+1), lick_per_trial_count[indices_lick_count[0]:indices_lick_count[1]+1], marker='o', color='orange')
         textstr_disengagement = f"Found {indices_lick_count[1] - indices_lick_count[0] + 1} consecutive trials with licks < 2\nStarting at index {indices_lick_count[0]}, ending at index {indices_lick_count[1]}"
-        ax5.text(0.05, 2.5, textstr_disengagement, fontsize=11, verticalalignment='bottom')
+        ax5.text(0.05, 0, textstr_disengagement, fontsize=11, verticalalignment='bottom')
     else:
         ax5.set_title('Lick Count', fontsize=11)
         textstr_disengagement = f"Still Engaged"
-        ax5.text(0.05, 2.5, textstr_disengagement, fontsize=11, verticalalignment='bottom')
+        ax5.text(0.05, 0, textstr_disengagement, fontsize=11, verticalalignment='bottom')
 
     ########################################################################
     # create the d' figure
