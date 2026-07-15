a960a409a5b3c73eb69a9033b29594e685f2347d
juliabenville <julia.benville@einsteinmed.edu>
2026-07-15 09:59:55 -0400

    baseline changes

--- behavbox_DT.py
+++ behavbox_DT.py
@@ -57,10 +57,9 @@ class BehavBox(object):
             print(str(error_message))
 
         from subprocess import check_output
-        IP_address = check_output(['hostname', '-I']).decode('ascii')[:-2]
+        IP_address = check_output(['hostname', '-I']).decode('ascii').strip().split()[0]
         self.IP_address = IP_address
         IP_address_video_list = list(IP_address)
-        # IP_address_video_list[-3] = "2"
         IP_address_video_list[-1] = "2"
         self.IP_address_video = "".join(IP_address_video_list)
 
